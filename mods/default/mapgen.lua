492e86050cec47dc928cb1528c80a0970488109b
Perttu Ahola <celeron55@gmail.com>
2012-03-29 13:44:56 +0300

    Clean/fix clay generation, update tool definitions to suit the latest engine

--- mods/default/mapgen.lua
+++ mods/default/mapgen.lua
@@ -9,17 +9,21 @@ local function generate_ore(name, wherein, minp, maxp, seed, chunks_per_volume, 
 	local volume = (maxp.x-minp.x+1)*(y_max-y_min+1)*(maxp.z-minp.z+1)
 	local pr = PseudoRandom(seed)
 	local num_chunks = math.floor(chunks_per_volume * volume)
-	local inverse_chance = math.floor(27 / ore_per_chunk)
+	local chunk_size = 3
+	if ore_per_chunk <= 4 then
+		chunk_size = 2
+	end
+	local inverse_chance = math.floor(chunk_size*chunk_size*chunk_size / ore_per_chunk)
 	--print("generate_ore num_chunks: "..dump(num_chunks))
 	for i=1,num_chunks do
-		local y0 = pr:next(y_min, y_max)
+		local y0 = pr:next(y_min, y_max-chunk_size+1)
 		if y0 >= height_min and y0 <= height_max then
-			local x0 = pr:next(minp.x, maxp.x)
-			local z0 = pr:next(minp.z, maxp.z)
+			local x0 = pr:next(minp.x, maxp.x-chunk_size+1)
+			local z0 = pr:next(minp.z, maxp.z-chunk_size+1)
 			local p0 = {x=x0, y=y0, z=z0}
-			for x1=-1,1 do
-			for y1=-1,1 do
-			for z1=-1,1 do
+			for x1=0,chunk_size-1 do
+			for y1=0,chunk_size-1 do
+			for z1=0,chunk_size-1 do
 				if pr:next(1,inverse_chance) == 1 then
 					local x2 = x0+x1
 					local y2 = y0+y1
@@ -38,35 +42,42 @@ local function generate_ore(name, wherein, minp, maxp, seed, chunks_per_volume, 
 end
 
 minetest.register_on_generated(function(minp, maxp, seed)
-	generate_ore("default:stone_with_coal", "default:stone", minp, maxp, seed,   1/8/8/8, 5, -64, 64)
-	generate_ore("default:stone_with_iron", "default:stone", minp, maxp, seed+1, 1/16/16/16, 5, 3, 7)
-	generate_ore("default:stone_with_iron", "default:stone", minp, maxp, seed+2, 1/12/12/12, 5, -16, 2)
-	generate_ore("default:stone_with_iron", "default:stone", minp, maxp, seed+3, 1/9/9/9, 5, -64, -17)
+	generate_ore("default:stone_with_coal", "default:stone", minp, maxp, seed,   1/8/8/8,    5, -64,  64)
+	generate_ore("default:stone_with_iron", "default:stone", minp, maxp, seed+1, 1/16/16/16, 5,   3,   7)
+	generate_ore("default:stone_with_iron", "default:stone", minp, maxp, seed+2, 1/12/12/12, 5, -16,   2)
+	generate_ore("default:stone_with_iron", "default:stone", minp, maxp, seed+3, 1/9/9/9,    5, -64, -17)
 	-- Generate clay
 	if maxp.y >= 2 and minp.y <= 0 then
 		-- Assume X and Z lengths are equal
 		local divlen = 4
 		local divs = (maxp.x-minp.x)/divlen+1;
-		for divx=1,divs do
-		for divz=1,divs do
+		for divx=0+1,divs-1-1 do
+		for divz=0+1,divs-1-1 do
 			local cx = minp.x + math.floor((divx+0.5)*divlen)
 			local cz = minp.z + math.floor((divz+0.5)*divlen)
-			local is_shallow = true
-			if minetest.env:get_node({x=cx-divlen*2,y=1,z=cz+0}).name == "default:water_source" or
-					minetest.env:get_node({x=cx+divlen*2,y=1,z=cz+0}).name == "default:water_source" or
-					minetest.env:get_node({x=cx,y=1,z=cz-divlen*2}).name == "default:water_source" or
-					minetest.env:get_node({x=cx,y=1,z=cz+divlen*2}).name == "default:water_source" then
-				is_shallow = false
-			end	
-			if is_shallow and
-					minetest.env:get_node({x=cx,y=1,z=cz}).name == "default:water_source" and
+			if minetest.env:get_node({x=cx,y=1,z=cz}).name == "default:water_source" and
 					minetest.env:get_node({x=cx,y=0,z=cz}).name == "default:sand" then
-				for x1=-divlen,divlen do
-				for z1=-divlen,divlen do
-					if minetest.env:get_node({x=cx+x1,y=0,z=cz+z1}).name == "default:sand" then
-						minetest.env:set_node({x=cx+x1,y=0,z=cz+z1}, {name="default:clay"})
+				local is_shallow = true
+				local num_water_around = 0
+				if minetest.env:get_node({x=cx-divlen*2,y=1,z=cz+0}).name == "default:water_source" then
+					num_water_around = num_water_around + 1 end
+				if minetest.env:get_node({x=cx+divlen*2,y=1,z=cz+0}).name == "default:water_source" then
+					num_water_around = num_water_around + 1 end
+				if minetest.env:get_node({x=cx+0,y=1,z=cz-divlen*2}).name == "default:water_source" then
+					num_water_around = num_water_around + 1 end
+				if minetest.env:get_node({x=cx+0,y=1,z=cz+divlen*2}).name == "default:water_source" then
+					num_water_around = num_water_around + 1 end
+				if num_water_around >= 2 then
+					is_shallow = false
+				end	
+				if is_shallow then
+					for x1=-divlen,divlen do
+					for z1=-divlen,divlen do
+						if minetest.env:get_node({x=cx+x1,y=0,z=cz+z1}).name == "default:sand" then
+							minetest.env:set_node({x=cx+x1,y=0,z=cz+z1}, {name="default:clay"})
+						end
 					end
-				end
+					end
 				end
 			end
 		end
