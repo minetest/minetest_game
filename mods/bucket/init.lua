cd4e86773f81cc54349d8111f452a4502bd67b68
Perttu Ahola <celeron55@gmail.com>
2012-07-25 18:28:21 +0300

    Add description to bucket to make it show in creative inventory

--- mods/bucket/init.lua
+++ mods/bucket/init.lua
@@ -56,6 +56,7 @@ function bucket.register_liquid(source, flowing, itemname, inventory_image)
 end
 
 minetest.register_craftitem("bucket:bucket_empty", {
+	description = "Emtpy bucket",
 	inventory_image = "bucket.png",
 	stack_max = 1,
 	liquids_pointable = true,
