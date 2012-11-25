5660c99707dfd8bbb17f907ea58c87f38a35629c
PilzAdam <adam-k@outlook.com>
2012-11-25 14:18:06 +0100

    Add animation for fire

--- mods/fire/init.lua
+++ mods/fire/init.lua
@@ -2,8 +2,12 @@
 
 minetest.register_node("fire:basic_flame", {
 	description = "Fire",
-	drawtype = "glasslike",
-	tiles = {"fire_basic_flame.png"},
+	drawtype = "plantlike",
+	tiles = {{
+		name="fire_basic_flame_animated.png",
+		animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=1},
+	}},
+	inventory_image = "fire_basic_flame.png",
 	light_source = 14,
 	groups = {igniter=2,dig_immediate=3},
 	drop = '',
