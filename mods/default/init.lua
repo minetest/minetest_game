2817db19d40908dc68c3bb0ddb78f626ec4b48e6
Jat <jinan15@hotmail.com>
2013-02-06 02:43:48 +0100

    Fix duplicate in default

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -829,7 +829,7 @@ minetest.register_node("default:desert_sand", {
 	description = "Desert Sand",
 	tiles = {"default_desert_sand.png"},
 	is_ground_content = true,
-	groups = {sand=1, crumbly=3, falling_node=1, sand=1},
+	groups = {crumbly=3, falling_node=1, sand=1},
 	sounds = default.node_sound_sand_defaults(),
 })
 
