873e33ec7002ac63cc1050c839313d3399ef125e
Casimir <jul.lutz@gmx.net>
2013-03-10 22:54:47 +0100

    Make sandstone only craftable with normal sand
    With group:sand it would be possible to craft 4 desertsand ---> sandstone ---> 4 normal sand. Therefor only use default:sand.

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -417,8 +417,8 @@ minetest.register_craft({
 minetest.register_craft({
 	output = 'default:sandstone',
 	recipe = {
-		{'group:sand', 'group:sand'},
-		{'group:sand', 'group:sand'},
+		{'default:sand', 'default:sand'},
+		{'default:sand', 'default:sand'},
 	}
 })
 
