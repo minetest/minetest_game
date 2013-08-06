804a9d6ffe8857a52b94c6559f7ec8394371b1c6
PilzAdam <pilzadam@minetest.net>
2013-08-06 15:39:22 +0200

    Longer range and insta-dig in creative

--- mods/creative/init.lua
+++ mods/creative/init.lua
@@ -127,15 +127,16 @@ if minetest.setting_getbool("creative_mode") then
 		type = "none",
 		wield_image = "wieldhand.png",
 		wield_scale = {x=1,y=1,z=2.5},
+		range = 10,
 		tool_capabilities = {
 			full_punch_interval = 0.5,
 			max_drop_level = 3,
 			groupcaps = {
-				crumbly = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
-				cracky = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
-				snappy = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
-				choppy = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
-				oddly_breakable_by_hand = {times={[1]=0.5, [2]=0.5, [3]=0.5}, uses=0, maxlevel=3},
+				crumbly = {times={[1]=0.0, [2]=0.0, [3]=0.0}, uses=0, maxlevel=3},
+				cracky = {times={[1]=0.0, [2]=0.0, [3]=0.0}, uses=0, maxlevel=3},
+				snappy = {times={[1]=0.0, [2]=0.0, [3]=0.0}, uses=0, maxlevel=3},
+				choppy = {times={[1]=0.0, [2]=0.0, [3]=0.0}, uses=0, maxlevel=3},
+				oddly_breakable_by_hand = {times={[1]=0.0, [2]=0.0, [3]=0.0}, uses=0, maxlevel=3},
 			},
 			damage_groups = {fleshy = 10},
 		}
