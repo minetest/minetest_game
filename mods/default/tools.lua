672fdbcc11386ebd095eb5fccfbb73c108b9616e
PilzAdam <pilzadam@minetest.net>
2013-09-29 00:02:38 +0200

    Fix stone and wooden axe being slower than hand

--- mods/default/tools.lua
+++ mods/default/tools.lua
@@ -11,7 +11,7 @@ minetest.register_item(":", {
 		groupcaps = {
 			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
 			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
-			oddly_breakable_by_hand = {times={[1]=7.00,[2]=4.00,[3]=1.40}, uses=0, maxlevel=3}
+			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
 		},
 		damage_groups = {fleshy=1},
 	}
