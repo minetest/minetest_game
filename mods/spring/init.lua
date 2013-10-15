daba3783b3847c77a1001affd659eebad0cdf271
proller <proller@github.com>
2013-10-15 16:38:14 +0400

    spring fix

--- mods/spring/init.lua
+++ mods/spring/init.lua
@@ -1,27 +1,27 @@
-minetest.register_node("default:water_spring",{
+minetest.register_node("spring:water_spring",{
     description = "Water Spring",
     tiles = {"default_water.png"},
 })
 
-minetest.register_node("default:lava_spring",{
+minetest.register_node("spring:lava_spring",{
     description = "Lava Spring",
     tiles = {"default_lava.png"},
 })
 
 -- if minetest.setting_getbool("liquid_finite") then
 minetest.register_abm({
-    nodenames = {"default:water_spring"},
-    interval = 1,
-    chance = 1,
+    nodenames = {"spring:water_spring"},
+    interval = 10,
+    chance = 2,
     action = function(pos,node)
         minetest.env:set_node(pos,{name = "default:water_source", param2=128})
     end
 })
 
 minetest.register_abm({
-    nodenames = {"default:lava_spring"},
-    interval = 1,
-    chance = 1,
+    nodenames = {"spring:lava_spring"},
+    interval = 10,
+    chance = 2,
     action = function(pos,node)
         minetest.env:set_node(pos,{name = "default:lava_source", param2=128})
     end
