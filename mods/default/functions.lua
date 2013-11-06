9fdbc1f4072dcf2296af9bcce6ec92bfa75972ca
BlockMen <nmuelll@web.de>
2013-11-06 23:41:49 +0100

    Fix tree growing and jungle sapling growth rate

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -126,6 +126,49 @@ function on_punchnode(p, node)
 end
 minetest.register_on_punchnode(on_punchnode)
 
+
+--
+-- Grow trees
+--
+
+minetest.register_abm({
+        nodenames = {"default:sapling"},
+        interval = 10,
+        chance = 50,
+        action = function(pos, node)
+                local is_soil = minetest.registered_nodes[minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z}).name].groups.soil
+                if is_soil == nil or is_soil == 0 then return end
+                print("A sapling grows into a tree at "..minetest.pos_to_string(pos))
+                local vm = minetest.get_voxel_manip()
+                local minp, maxp = vm:read_from_map({x=pos.x-16, y=pos.y, z=pos.z-16}, {x=pos.x+16, y=pos.y+16, z=pos.z+16})
+                local a = VoxelArea:new{MinEdge=minp, MaxEdge=maxp}
+                local data = vm:get_data()
+                default.grow_tree(data, a, pos, math.random(1, 4) == 1, math.random(1,100000))
+                vm:set_data(data)
+                vm:write_to_map(data)
+                vm:update_map()
+        end
+})
+
+minetest.register_abm({
+        nodenames = {"default:junglesapling"},
+        interval = 10,
+        chance = 50,
+        action = function(pos, node)
+                local is_soil = minetest.registered_nodes[minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z}).name].groups.soil
+                if is_soil == nil or is_soil == 0 then return end
+                print("A jungle sapling grows into a tree at "..minetest.pos_to_string(pos))
+                local vm = minetest.get_voxel_manip()
+                local minp, maxp = vm:read_from_map({x=pos.x-16, y=pos.y-1, z=pos.z-16}, {x=pos.x+16, y=pos.y+16, z=pos.z+16})
+                local a = VoxelArea:new{MinEdge=minp, MaxEdge=maxp}
+                local data = vm:get_data()
+                default.grow_jungletree(data, a, pos, math.random(1,100000))
+                vm:set_data(data)
+                vm:write_to_map(data)
+                vm:update_map()
+        end
+})
+
 --
 -- Lavacooling
 --
