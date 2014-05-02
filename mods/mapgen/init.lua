ad8288344e537ef624db49b6988f00c07c07fd1d
Amaz1 <charliejordan34@gmail.com>
2014-05-02 21:42:57 +0100

    Add vacuum evrywhere and always use mapgen 7

--- mods/mapgen/init.lua
+++ mods/mapgen/init.lua
@@ -1,3 +1,12 @@
+minetest.register_on_mapgen_init(function(params)
+	minetest.set_mapgen_params({
+		mgname = "v7",
+		seed = params.seed,
+		water_level = 31000,
+		flags = "caves",
+	})
+end)
+
 dofile(minetest.get_modpath("mapgen").."/mapgen.lua")
 
 minetest.register_alias("mapgen_water_source", "mapgen:vacuum")
