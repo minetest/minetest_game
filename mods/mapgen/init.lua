843c5dc8876991d77ab538910905d66bd15ee35a
ManOfMinetest24 <zsolt5000@gmail.com>
2014-05-02 15:19:57 -0600

    Update init.lua

--- mods/mapgen/init.lua
+++ mods/mapgen/init.lua
@@ -3,7 +3,7 @@ dofile(minetest.get_modpath("mapgen").."/mapgen.lua")
 minetest.register_alias("mapgen_water_source", "mapgen:vacuum")
 minetest.register_alias("mapgen_lava_source", "default:lava_source")
 minetest.register_alias("mapgen_stone", "mapgen:stone")
-minetest.register_alias("mapgen_dirt", "mapgen:dust")
+minetest.register_alias("mapgen_dirt", "mapgen:compressed_dust")
 minetest.register_alias("mapgen_dirt_with_grass", "air")
 
 minetest.register_node("mapgen:stone", {
