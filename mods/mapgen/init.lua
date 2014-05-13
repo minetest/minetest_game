a62e9b85d42c4a157b5412c40c7b0cad4f7f29a1
Craig <craig.davison3@gmail.com>
2014-05-13 21:25:59 +0100

    Mapgen tweaks

--- mods/mapgen/init.lua
+++ mods/mapgen/init.lua
@@ -1,3 +1,6 @@
+-- Mapgen
+
+-- Set mapgen mode to v7
 minetest.register_on_mapgen_init(function(params)
 	minetest.set_mapgen_params({
 		mgname = "v7",
@@ -7,11 +10,31 @@ minetest.register_on_mapgen_init(function(params)
 	})
 end)
 
-dofile(minetest.get_modpath("mapgen").."/mapgen.lua")
+-- Dust Biome
+minetest.register_biome({
+	name = "plains",
+	node_top = "moontest:dust",
+	node_bottom = "moontest:stone",
+	depth_top = 2,
+	node_dust = "air",
+	height_min = -10,
+	height_max = 160,
+})
+
+-- Obsidian Biome
+minetest.register_biome({
+	name = "plains",
+	node_top = "default:obsidian",
+	node_bottom = "moontest:stone",
+	depth_top = 8,
+	node_dust = "air",
+	height_min = -30,
+	height_max = 20,
+})
+
+-- Aliases
 
 minetest.register_alias("mapgen_water_source", "moontest:vacuum")
 minetest.register_alias("mapgen_lava_source", "default:lava_source")
 minetest.register_alias("mapgen_stone", "moontest:stone")
-minetest.register_alias("mapgen_dirt", "moontest:compressed_dust")
-minetest.register_alias("mapgen_dirt_with_grass", "air")
-
+minetest.register_alias("mapgen_dirt", "moontest:dust")
