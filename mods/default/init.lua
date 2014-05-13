631450c05e36e47deda40ca7bd075f9212ad1747
Craig <craig.davison3@gmail.com>
2014-05-13 21:45:16 +0100

    Clean up some default code

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -1,21 +1,111 @@
 -- Minetest 0.4 mod: default
 -- See README.txt for licensing and other information.
 
--- The API documentation in here was moved into doc/lua_api.txt
-
 WATER_ALPHA = 160
 WATER_VISC = 1
 LAVA_VISC = 7
 LIGHT_MAX = 14
 
--- Definitions made by this mod that other mods can use too
 default = {}
 
+-- Hand
+minetest.register_item(":", {
+	type = "none",
+	wield_image = "wieldhand.png",
+	wield_scale = {x=1,y=1,z=2.5},
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level = 0,
+		groupcaps = {
+			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
+		},
+		damage_groups = {fleshy=1},
+	}
+})
+
+-- Sounds
+function default.node_sound_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name="", gain=1.0}
+	table.dug = table.dug or
+			{name="default_dug_node", gain=0.25}
+	table.place = table.place or
+			{name="default_place_node_hard", gain=1.0}
+	return table
+end
+
+function default.node_sound_stone_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name="default_hard_footstep", gain=0.5}
+	table.dug = table.dug or
+			{name="default_hard_footstep", gain=1.0}
+	default.node_sound_defaults(table)
+	return table
+end
+
+function default.node_sound_dirt_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name="default_dirt_footstep", gain=1.0}
+	table.dug = table.dug or
+			{name="default_dirt_footstep", gain=1.5}
+	table.place = table.place or
+			{name="default_place_node", gain=1.0}
+	default.node_sound_defaults(table)
+	return table
+end
+
+function default.node_sound_sand_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name="default_sand_footstep", gain=0.5}
+	table.dug = table.dug or
+			{name="default_sand_footstep", gain=1.0}
+	table.place = table.place or
+			{name="default_place_node", gain=1.0}
+	default.node_sound_defaults(table)
+	return table
+end
+
+function default.node_sound_wood_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name="default_wood_footstep", gain=0.5}
+	table.dug = table.dug or
+			{name="default_wood_footstep", gain=1.0}
+	default.node_sound_defaults(table)
+	return table
+end
+
+function default.node_sound_leaves_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name="default_grass_footstep", gain=0.35}
+	table.dug = table.dug or
+			{name="default_grass_footstep", gain=0.85}
+	table.dig = table.dig or
+			{name="default_dig_crumbly", gain=0.4}
+	table.place = table.place or
+			{name="default_place_node", gain=1.0}
+	default.node_sound_defaults(table)
+	return table
+end
+
+function default.node_sound_glass_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name="default_glass_footstep", gain=0.5}
+	table.dug = table.dug or
+			{name="default_break_glass", gain=1.0}
+	default.node_sound_defaults(table)
+	return table
+end
+
 -- Load files
-dofile(minetest.get_modpath("default").."/functions.lua")
 dofile(minetest.get_modpath("default").."/nodes.lua")
-dofile(minetest.get_modpath("default").."/tools.lua")
 dofile(minetest.get_modpath("default").."/craftitems.lua")
-dofile(minetest.get_modpath("default").."/crafting.lua")
 dofile(minetest.get_modpath("default").."/player.lua")
-dofile(minetest.get_modpath("default").."/trees.lua")
