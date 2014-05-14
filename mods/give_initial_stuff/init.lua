54f4cecbeabf2c5abf346fea69434315d16e0084
Zsolt Bartha <zsolt5000@gmail.com>
2014-05-14 23:21:53 +0000

    Update init.lua

--- mods/give_initial_stuff/init.lua
+++ mods/give_initial_stuff/init.lua
@@ -1,6 +1,5 @@
 minetest.register_on_newplayer(function(player)
-	--print("on_newplayer")
-
+	print("on_newplayer")
 	minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
 	player:get_inventory():add_item('main', 'default:pick_steel')
 	player:get_inventory():add_item('main', 'default:torch 99')
