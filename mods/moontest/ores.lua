61e97e8f96093eea7e67c863e16c61995942a3b0
Amaz1 <charliejordan34@gmail.com>
2014-05-14 21:30:13 +0100

    Added light ore!

--- mods/moontest/ores.lua
+++
@@ -1,9 +0,0 @@
-minetest.register_node("moontest:lightore", {
-	description = "Lightore",
-	tiles = {"moontest_stone.png^moontest_light_ore.png"},
-	light_source = 7,
-	groups = {cracky = 3, stone = 1},
-	drop = "moontest:light_crystal",
-})
-
-
