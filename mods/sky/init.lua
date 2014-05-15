819f89634d33a3d0afda38db25ea8a98ad6032a4
Craig <craig.davison3@gmail.com>
2014-05-15 15:34:26 +0100

    Change time of day

--- mods/sky/init.lua
+++ mods/sky/init.lua
@@ -1,11 +1,11 @@
 
 -- Set time to midnight.
 minetest.register_on_joinplayer(function(player)
-    minetest.set_timeofday(0)
+    minetest.set_timeofday(5000)
     minetest.setting_set("time_speed", 0)
 end)
 
--- Disable clouds and enable them again when player leaves the game.
+-- Disable clouds and enable them again when player leaves the game. (Experimental)
 minetest.register_on_joinplayer(function(player)
     minetest.setting_set("enable_clouds", 0)
 end)
