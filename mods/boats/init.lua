c993e14084553f01b4f5538a72f2bb2388be9b11
Novatux <nathanael.courant@laposte.net>
2014-07-09 09:47:51 +0200

    Add spaces around operators in boat mod code, fix a problem with boat staticdata, fix a crash that can occur with boat going over unknown nodes.

--- mods/boats/init.lua
+++ mods/boats/init.lua
@@ -12,18 +12,18 @@ local function get_sign(i)
 	if i == 0 then
 		return 0
 	else
-		return i/math.abs(i)
+		return i / math.abs(i)
 	end
 end
 
 local function get_velocity(v, yaw, y)
-	local x = -math.sin(yaw)*v
-	local z = math.cos(yaw)*v
-	return {x=x, y=y, z=z}
+	local x = -math.sin(yaw) * v
+	local z =  math.cos(yaw) * v
+	return {x = x, y = y, z = z}
 end
 
 local function get_v(v)
-	return math.sqrt(v.x^2+v.z^2)
+	return math.sqrt(v.x ^ 2 + v.z ^ 2)
 end
 
 --
@@ -32,7 +32,7 @@ end
 
 local boat = {
 	physical = true,
-	collisionbox = {-0.6,-0.4,-0.6, 0.6,0.3,0.6},
+	collisionbox = {-0.6, -0.4, -0.6, 0.6, 0.3, 0.6},
 	visual = "mesh",
 	mesh = "boat.x",
 	textures = {"default_wood.png"},
@@ -55,25 +55,25 @@ function boat.on_rightclick(self, clicker)
 		default.player_set_animation(clicker, "stand" , 30)
 	elseif not self.driver then
 		self.driver = clicker
-		clicker:set_attach(self.object, "", {x=0,y=11,z=-3}, {x=0,y=0,z=0})
+		clicker:set_attach(self.object, "", {x = 0, y = 11, z = -3}, {x = 0, y = 0, z = 0})
 		default.player_attached[name] = true
 		minetest.after(0.2, function()
 			default.player_set_animation(clicker, "sit" , 30)
 		end)
-		self.object:setyaw(clicker:get_look_yaw()-math.pi/2)
+		self.object:setyaw(clicker:get_look_yaw() - math.pi / 2)
 	end
 end
 
 function boat.on_activate(self, staticdata, dtime_s)
-	self.object:set_armor_groups({immortal=1})
+	self.object:set_armor_groups({immortal = 1})
 	if staticdata then
 		self.v = tonumber(staticdata)
 	end
 	self.last_v = self.v
 end
 
-function boat.get_staticdata()
-	return tostring(v)
+function boat.get_staticdata(self)
+	return tostring(self.v)
 end
 
 function boat.on_punch(self, puncher, time_from_last_punch, tool_capabilities, direction)
@@ -85,7 +85,7 @@ function boat.on_punch(self, puncher, time_from_last_punch, tool_capabilities, d
 
 	self.removed = true
 	-- delay remove to ensure player is detached
-	minetest.after(0.1,function()
+	minetest.after(0.1, function()
 		self.object:remove()
 	end)
 	if not minetest.setting_getbool("creative_mode") then
@@ -94,28 +94,28 @@ function boat.on_punch(self, puncher, time_from_last_punch, tool_capabilities, d
 end
 
 function boat.on_step(self, dtime)
-	self.v = get_v(self.object:getvelocity())*get_sign(self.v)
+	self.v = get_v(self.object:getvelocity()) * get_sign(self.v)
 	if self.driver then
 		local ctrl = self.driver:get_player_control()
 		local yaw = self.object:getyaw()
 		if ctrl.up then
-			self.v = self.v+0.1
+			self.v = self.v + 0.1
 		end
 		if ctrl.down then
-			self.v = self.v-0.08
+			self.v = self.v - 0.08
 		end
 		if ctrl.left then
 			if ctrl.down then
-				self.object:setyaw(yaw-math.pi/120-dtime*math.pi/120)
+				self.object:setyaw(yaw - math.pi / 120 - dtime * math.pi / 120)
 			else
-				self.object:setyaw(yaw+math.pi/120+dtime*math.pi/120)
+				self.object:setyaw(yaw + math.pi / 120 + dtime * math.pi / 120)
 			end
 		end
 		if ctrl.right then
 			if ctrl.down then
-				self.object:setyaw(yaw+math.pi/120+dtime*math.pi/120)
+				self.object:setyaw(yaw + math.pi / 120 + dtime * math.pi / 120)
 			else
-				self.object:setyaw(yaw-math.pi/120-dtime*math.pi/120)
+				self.object:setyaw(yaw - math.pi / 120 - dtime*math.pi/120)
 			end
 		end
 	end
@@ -124,43 +124,44 @@ function boat.on_step(self, dtime)
 		return
 	end
 	local s = get_sign(self.v)
-	self.v = self.v - 0.02*s
+	self.v = self.v - 0.02 * s
 	if s ~= get_sign(self.v) then
-		self.object:setvelocity({x=0, y=0, z=0})
+		self.object:setvelocity({x = 0, y = 0, z = 0})
 		self.v = 0
 		return
 	end
 	if math.abs(self.v) > 4.5 then
-		self.v = 4.5*get_sign(self.v)
+		self.v = 4.5 * get_sign(self.v)
 	end
 
 	local p = self.object:getpos()
-	p.y = p.y-0.5
-	local new_velo = {x=0,y=0,z=0}
-	local new_acce = {x=0,y=0,z=0}
+	p.y = p.y - 0.5
+	local new_velo = {x = 0, y = 0, z = 0}
+	local new_acce = {x = 0, y = 0, z = 0}
 	if not is_water(p) then
-		if minetest.registered_nodes[minetest.env:get_node(p).name].walkable then
+		local nodedef = minetest.registered_nodes[minetest.get_node(p).name]
+		if (not nodedef) or nodedef.walkable then
 			self.v = 0
 		end
-		new_acce = {x=0, y=-10, z=0}
+		new_acce = {x = 0, y = -10, z = 0}
 		new_velo = get_velocity(self.v, self.object:getyaw(), self.object:getvelocity().y)
 	else
-		p.y = p.y+1
+		p.y = p.y + 1
 		if is_water(p) then
-			new_acce = {x=0, y=3, z=0}
+			new_acce = {x = 0, y = 3, z = 0}
 			local y = self.object:getvelocity().y
 			if y > 2 then
 				y = 2
 			end
 			if y < 0 then
-				self.object:setacceleration({x=0, y=10, z=0})
+				self.object:setacceleration({x = 0, y = 10, z = 0})
 			end
 			new_velo = get_velocity(self.v, self.object:getyaw(), y)
 		else
-			new_acce = {x=0, y=0, z=0}
+			new_acce = {x = 0, y = 0, z = 0}
 			if math.abs(self.object:getvelocity().y) < 1 then
 				local pos = self.object:getpos()
-				pos.y = math.floor(pos.y)+0.5
+				pos.y = math.floor(pos.y) + 0.5
 				self.object:setpos(pos)
 				new_velo = get_velocity(self.v, self.object:getyaw(), 0)
 			else
@@ -179,7 +180,7 @@ minetest.register_craftitem("boats:boat", {
 	description = "Boat",
 	inventory_image = "boat_inventory.png",
 	wield_image = "boat_wield.png",
-	wield_scale = {x=2, y=2, z=1},
+	wield_scale = {x = 2, y = 2, z = 1},
 	liquids_pointable = true,
 
 	on_place = function(itemstack, placer, pointed_thing)
@@ -189,7 +190,7 @@ minetest.register_craftitem("boats:boat", {
 		if not is_water(pointed_thing.under) then
 			return
 		end
-		pointed_thing.under.y = pointed_thing.under.y+0.5
+		pointed_thing.under.y = pointed_thing.under.y + 0.5
 		minetest.add_entity(pointed_thing.under, "boats:boat")
 		if not minetest.setting_getbool("creative_mode") then
 			itemstack:take_item()
@@ -201,8 +202,8 @@ minetest.register_craftitem("boats:boat", {
 minetest.register_craft({
 	output = "boats:boat",
 	recipe = {
-		{"", "", ""},
-		{"group:wood", "", "group:wood"},
+		{"",           "",           ""          },
+		{"group:wood", "",           "group:wood"},
 		{"group:wood", "group:wood", "group:wood"},
 	},
 })
