3180bdfe6ce40a6f155329078884e797c310b8f1
BlockMen <nmuelll@web.de>
2014-07-13 19:20:20 +0200

    Add protection to TNT (by @tenplus1)

--- mods/tnt/init.lua
+++ mods/tnt/init.lua
@@ -37,7 +37,11 @@ local add_drop = function(drops, pos, item)
 	end
 end
 
-local destroy = function(drops, pos, last, fast)
+local function destroy(drops, pos, last, fast)
+	if minetest.is_protected(pos, "") then
+		return
+	end
+
 	local nodename = minetest.get_node(pos).name
 	if nodename ~= "air" then
 		minetest.remove_node(pos, (fast and 1 or 0))
