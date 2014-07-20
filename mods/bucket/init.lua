263b6f2fdf40fa1e2397cb4ae68a12bd76f4c48c
Zefram <zefram@fysh.org>
2014-07-20 17:27:58 +0100

    Show filled buckets in creative inventory

--- mods/bucket/init.lua
+++ mods/bucket/init.lua
@@ -51,7 +51,6 @@ function bucket.register_liquid(source, flowing, itemname, inventory_image, name
 			inventory_image = inventory_image,
 			stack_max = 1,
 			liquids_pointable = true,
-			groups = {not_in_creative_inventory=1},
 			on_place = function(itemstack, user, pointed_thing)
 				-- Must be pointing to node
 				if pointed_thing.type ~= "node" then
