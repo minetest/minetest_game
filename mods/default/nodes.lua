ed7389b15a8083a02dff87ec62caeae42fa99826
swaaws <swaaws@users.noreply.github.com>
2014-07-31 13:22:08 +0200

    Update cactus
    line 351: dirt can fly ^^ but i have never seen flamable cactus
    line 353: I have more than once in life killed me with cactus

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -348,8 +348,9 @@ minetest.register_node("default:cactus", {
 	tiles = {"default_cactus_top.png", "default_cactus_top.png", "default_cactus_side.png"},
 	paramtype2 = "facedir",
 	is_ground_content = true,
-	groups = {snappy=1,choppy=3,flammable=2},
+	groups = {snappy=1,choppy=3},
 	sounds = default.node_sound_wood_defaults(),
+	damage_per_second = 4,
 	on_place = minetest.rotate_node,
 	after_dig_node = function(pos, node, metadata, digger)
 		default.dig_up(pos, node, digger)
