b27fae1eb60691e19138a11ca7ee9474aec6b492
paramat <mat.gregory@virginmedia.com>
2014-10-21 18:53:04 +0100

    Pine tree, pine needles, pine sapling and pinewood nodes, including textures by Splizard and Cisoun

--- mods/default/crafting.lua
+++ mods/default/crafting.lua
@@ -15,6 +15,13 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
+	output = 'default:pinewood 4',
+	recipe = {
+		{'default:pinetree'},
+	}
+})
+
+minetest.register_craft({
 	output = 'default:stick 4',
 	recipe = {
 		{'group:wood'},
@@ -800,3 +807,10 @@ minetest.register_craft({
 	recipe = "default:grass_1",
 	burntime = 2,
 })
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "default:pine_sapling",
+	burntime = 10,
+})
+
