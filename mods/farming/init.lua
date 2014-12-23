09d50906aedce55fc2d914ce5c29e4cf8ea80bbc
jp <jeanpatrick.guerrero@gmail.com>
2014-12-23 16:02:09 +0100

    Add straw

--- mods/farming/init.lua
+++ mods/farming/init.lua
@@ -59,3 +59,12 @@ minetest.register_craft({
 		{"farming:cotton", "farming:cotton"},
 	}
 })
+
+minetest.register_craft({
+	output = "farming:straw",
+	recipe = {
+		{"farming:wheat", "farming:wheat", "farming:wheat"},
+		{"farming:wheat", "farming:wheat", "farming:wheat"},
+		{"farming:wheat", "farming:wheat", "farming:wheat"},
+	}
+})
