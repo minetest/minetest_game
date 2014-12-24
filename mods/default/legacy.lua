51be92cd9890a953212ab1b4ee604096ed92b6b7
Craig Davison <craig.davison3@gmail.com>
2014-12-24 23:13:15 +0000

    Fix typo in survival formspec & create legacy file

---
+++ mods/default/legacy.lua
@@ -0,0 +1,25 @@
+-- mods/default/legacy.lua
+
+-- Horrible crap to support old code registering falling nodes
+-- Don't use this and never do what this does, it's completely wrong!
+-- (More specifically, the client and the C++ code doesn't get the group)
+function default.register_falling_node(nodename, texture)
+	minetest.log("error", debug.traceback())
+	minetest.log('error', "WARNING: default.register_falling_node is deprecated")
+	if minetest.registered_nodes[nodename] then
+		minetest.registered_nodes[nodename].groups.falling_node = 1
+	end
+end
+
+function default.spawn_falling_node(p, nodename)
+	spawn_falling_node(p, nodename)
+end
+
+-- Liquids
+WATER_ALPHA = minetest.registered_nodes["default:water_source"].alpha
+WATER_VISC = minetest.registered_nodes["default:water_source"].liquid_viscosity
+LAVA_VISC = minetest.registered_nodes["default:lava_source"].liquid_viscosity
+LIGHT_MAX = default.LIGHT_MAX
+
+-- Formspecs
+default.gui_suvival_form = default.gui_survival_form
