532e585e926fc6c8a64d0ad9e047d57386baa4fe
Wuzzy <almikes@aol.com>
2015-05-13 02:42:06 +0200

    Use builtin function for raillike group

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -13,10 +13,6 @@ default.gui_bg = "bgcolor[#080808BB;true]"
 default.gui_bg_img = "background[5,5;1,1;gui_formbg.png;true]"
 default.gui_slots = "listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]"
 
--- connect_to_raillike group values
-default.connect_to_raillike = {}
-default.connect_to_raillike.rails = 2
-
 function default.get_hotbar_bg(x,y)
 	local out = ""
 	for i=0,7,1 do
