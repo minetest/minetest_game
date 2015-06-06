750f9575af15d92abc397d99aba90005fd3cade8
Calinou <calinou@opmbx.org>
2015-06-06 13:57:52 +0200

    Improve water post effect color

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -916,7 +916,7 @@ minetest.register_node("default:water_source", {
 	liquid_alternative_flowing = "default:water_flowing",
 	liquid_alternative_source = "default:water_source",
 	liquid_viscosity = 1,
-	post_effect_color = {a=64, r=100, g=100, b=200},
+	post_effect_color = {a=120, r=30, g=60, b=90},
 	groups = {water=3, liquid=3, puts_out_fire=1},
 })
 
@@ -961,7 +961,7 @@ minetest.register_node("default:water_flowing", {
 	liquid_alternative_flowing = "default:water_flowing",
 	liquid_alternative_source = "default:water_source",
 	liquid_viscosity = 1,
-	post_effect_color = {a=64, r=100, g=100, b=200},
+	post_effect_color = {a=120, r=30, g=60, b=90},
 	groups = {water=3, liquid=3, puts_out_fire=1, not_in_creative_inventory=1},
 })
 
@@ -1008,7 +1008,7 @@ minetest.register_node("default:river_water_source", {
 	liquid_viscosity = 1,
 	liquid_renewable = false,
 	liquid_range = 2,
-	post_effect_color = {a=64, r=100, g=178, b=200},
+	post_effect_color = {a=120, r=30, g=60, b=90},
 	groups = {water=3, liquid=3, puts_out_fire=1},
 })
 
@@ -1055,7 +1055,7 @@ minetest.register_node("default:river_water_flowing", {
 	liquid_viscosity = 1,
 	liquid_renewable = false,
 	liquid_range = 2,
-	post_effect_color = {a=64, r=100, g=178, b=200},
+	post_effect_color = {a=120, r=30, g=60, b=90},
 	groups = {water=3, liquid=3, puts_out_fire=1, not_in_creative_inventory=1},
 })
 
