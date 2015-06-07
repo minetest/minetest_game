8f90e07d85832c376316d3ff35c3f169da222ab9
paramat <mat.gregory@virginmedia.com>
2015-06-07 02:59:35 +0100

    Default/nodes: Match river_water post_effect_colour hue to that of new texture

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -1008,7 +1008,7 @@ minetest.register_node("default:river_water_source", {
 	liquid_viscosity = 1,
 	liquid_renewable = false,
 	liquid_range = 2,
-	post_effect_color = {a=120, r=30, g=60, b=90},
+	post_effect_color = {a=120, r=30, g=76, b=90},
 	groups = {water=3, liquid=3, puts_out_fire=1},
 })
 
@@ -1055,7 +1055,7 @@ minetest.register_node("default:river_water_flowing", {
 	liquid_viscosity = 1,
 	liquid_renewable = false,
 	liquid_range = 2,
-	post_effect_color = {a=120, r=30, g=60, b=90},
+	post_effect_color = {a=120, r=30, g=76, b=90},
 	groups = {water=3, liquid=3, puts_out_fire=1, not_in_creative_inventory=1},
 })
 
