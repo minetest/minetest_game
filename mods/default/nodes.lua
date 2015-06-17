6e1a3297636bc0e20d623f0a3214144158fddb66
tenplus1 <tenplus1@users.noreply.github.com>
2015-06-17 18:07:55 +0100

    default: Strengthen obsidian glass Remove group 'oddly_breakable_by_hand'

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -1757,7 +1757,7 @@ minetest.register_node("default:obsidian_glass", {
 	is_ground_content = false,
 	sunlight_propagates = true,
 	sounds = default.node_sound_glass_defaults(),
-	groups = {cracky = 3, oddly_breakable_by_hand = 3},
+	groups = {cracky = 3},
 })
 
 
