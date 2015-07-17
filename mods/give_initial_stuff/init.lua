9c0e6995544d52d5161f085ba9bb843868ae4af2
Rui <Rui914@users.noreply.github.com>
2015-07-17 20:13:02 +0900

    Add items table for give_initial_stuff

--- mods/give_initial_stuff/init.lua
+++ mods/give_initial_stuff/init.lua
@@ -1,12 +1,17 @@
+local items = {
+	"default:pick_steel",
+	"default:axe_steel",
+	"default:shovel_steel",
+	"default:torch 99",
+	"default:cobble 99"
+}
+
 minetest.register_on_newplayer(function(player)
-	--print("on_newplayer")
 	if minetest.setting_getbool("give_initial_stuff") then
 		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
-		player:get_inventory():add_item('main', 'default:pick_steel')
-		player:get_inventory():add_item('main', 'default:torch 99')
-		player:get_inventory():add_item('main', 'default:axe_steel')
-		player:get_inventory():add_item('main', 'default:shovel_steel')
-		player:get_inventory():add_item('main', 'default:cobble 99')
+		local inv = player:get_inventory()
+		for _, item in ipairs(items) do
+			inv:add_item("main", item)
+		end
 	end
 end)
-
