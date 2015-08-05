ea62c801d5b880796d82f871d2ec87445355dfb9
Christian Wischenbart <c.wischenbart@bitoxide.com>
2015-08-05 22:20:41 +0200

    Fixed invalid check. This fixes minetest/minetest_game#609

--- mods/doors/init.lua
+++ mods/doors/init.lua
@@ -129,7 +129,7 @@ function doors.register_door(name, def)
 
 	local function on_rightclick(pos, dir, check_name, replace, replace_dir, params)
 		pos.y = pos.y+dir
-		if not minetest.get_node(pos).name == check_name then
+		if minetest.get_node(pos).name ~= check_name then
 			return
 		end
 		local p2 = minetest.get_node(pos).param2
