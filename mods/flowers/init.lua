d0fad4b004629fba5fadf0e01fafbe6413ecc648
paramat <mat.gregory@virginmedia.com>
2015-09-17 15:43:29 +0100

    Flowers: Make waterlily not walkable. Add missing flower group

--- mods/flowers/init.lua
+++ mods/flowers/init.lua
@@ -251,7 +251,8 @@ minetest.register_node("flowers:waterlily", {
 	inventory_image = "flowers_waterlily.png",
 	wield_image = "flowers_waterlily.png",
 	liquids_pointable = true,
-	groups = {snappy = 3},
+	walkable = false,
+	groups = {snappy = 3, flower = 1},
 	sounds = default.node_sound_leaves_defaults(),
 	node_box = {
 		type = "fixed",
