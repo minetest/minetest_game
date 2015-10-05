421929f80dc99f87c9c0b61afda7550646527056
paramat <mat.gregory@virginmedia.com>
2015-10-05 01:25:23 +0100

    Papyrus: Grow on dirt and grass only, remove from desert ocean

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -152,8 +152,7 @@ end
 function default.grow_papyrus(pos, node)
 	pos.y = pos.y - 1
 	local name = minetest.get_node(pos).name
-	if name ~= "default:dirt_with_grass" and name ~= "default:dirt" and
-			name ~= "default:sand" then
+	if name ~= "default:dirt_with_grass" and name ~= "default:dirt" then
 		return
 	end
 	if not minetest.find_node_near(pos, 3, {"group:water"}) then
