b998ab1b3ae8cb7f21fab30819a013b7d8e00aec
Auke Kok <sofar@foo-projects.org>
2016-02-26 07:30:38 -0800

    Add a simple, but stylish fence gate.
    This fence gate builds on NDT_CONNECTED by assuming fence nodes will
    automatically connect to it's sides properly. The fence gate will
    open and close just like doors, with sounds, but it only opens one
    way. The gate sticks out quite a bit and can be bumped into, so the
    fence may be used as some sort of path switch.
    
    The fence gate offers no form of protection and can be opened and
    closed by anyone. This is done on purpose - the fencegate isn't
    meant to provide protection from players, as fences can be
    trivially jumped over. Instead, these fences should be used for
    protecting crops from hungry sheep, or keeping rabbits in their
    pen, or just decoration. Mods can also modify the mod to add
    protection, of course.
    
    A recipe is added to make these. It's 4 sticks and 2 wood (any)
    as follows:
    
      stick  wood  stick
      stick  wood  stick
    
    The collision box of the open gate is such that if two gates are
    connected but mirrored (making an M shape) then you can walk a large
    entity that's larger than 1.0 wide through the opening. The gate of
    an opened fence can also be stood upon or bumped into.
    
    I've mixed together some sounds to provide a somewhat light sound
    experience, one that one would expect from a small gate latching open
    and close.
    
    This change requires #873, otherwise it doesn't connect to fences.

--- mods/doors/init.lua
+++ mods/doors/init.lua
@@ -642,3 +642,62 @@ minetest.register_craft({
 	}
 })
 
+
+----fence gate----
+
+local fence = {
+	description = "Fence Gate",
+	drawtype = "mesh",
+	tiles = {"default_wood.png"},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	is_ground_content = false,
+	drop = "doors:gate_closed",
+	connect_sides = { "left", "right" },
+	groups = {snappy = 1, choppy = 2, oddly_breakable_by_hand = 2,
+		flammable = 2, fence = 1},
+	on_rightclick = function(pos, clicker)
+		local node = minetest.get_node(pos)
+		local def = minetest.registered_nodes[node.name]
+		minetest.swap_node(pos, {name = def.gate, param2 = node.param2})
+		minetest.sound_play(def.sound, {pos = pos, gain = 0.3,
+			max_hear_distance = 8})
+	end,
+	selection_box = {
+		type = "fixed",
+		fixed = {-1/2, -1/2, -1/4, 1/2, 1/2, 1/4},
+	},
+}
+
+local fence_closed = table.copy(fence)
+fence_closed.mesh = "doors_fencegate_closed.obj"
+fence_closed.gate = "doors:gate_open"
+fence_closed.sound = "doors_fencegate_open"
+fence_closed.collision_box = {
+	type = "fixed",
+	fixed = {-1/2, -1/2, -1/4, 1/2, 1/2, 1/4},
+}
+
+local fence_open = table.copy(fence)
+fence_open.mesh = "doors_fencegate_open.obj"
+fence_open.gate = "doors:gate_closed"
+fence_open.sound = "doors_fencegate_close"
+fence_open.groups.not_in_creative_inventory = 1
+fence_open.collision_box = {
+	type = "fixed",
+	fixed = {{-1/2, -1/2, -1/4, -3/8, 1/2, 1/4},
+		{-5/8, -3/8, -14/16, -3/8, 3/8, 0}},
+}
+
+minetest.register_node("doors:gate_closed", fence_closed)
+minetest.register_node("doors:gate_open", fence_open)
+
+minetest.register_craft({
+	output = "doors:gate_closed",
+	recipe = {
+		{"default:stick", "group:wood", "default:stick"},
+		{"default:stick", "group:wood", "default:stick"},
+	},
+})
+
