7df1ba43d014182806b60b736e6bfa01509d2371
Emon <emon@openmailbox.org>
2016-05-16 14:22:31 +0200

    Tradotte le descrizioni

--- mods/farming/hoes.lua
+++ mods/farming/hoes.lua
@@ -1,40 +1,40 @@
 farming.register_hoe(":farming:hoe_wood", {
-	description = "Wooden Hoe",
+	description = "Zappa di legno",
 	inventory_image = "farming_tool_woodhoe.png",
 	max_uses = 30,
 	material = "group:wood"
 })
 
 farming.register_hoe(":farming:hoe_stone", {
-	description = "Stone Hoe",
+	description = "Zappa di pietra",
 	inventory_image = "farming_tool_stonehoe.png",
 	max_uses = 90,
 	material = "group:stone"
 })
 
 farming.register_hoe(":farming:hoe_steel", {
-	description = "Steel Hoe",
+	description = "Zappa di acciaio",
 	inventory_image = "farming_tool_steelhoe.png",
 	max_uses = 200,
 	material = "default:steel_ingot"
 })
 
 farming.register_hoe(":farming:hoe_bronze", {
-	description = "Bronze Hoe",
+	description = "Zappa di bronzo",
 	inventory_image = "farming_tool_bronzehoe.png",
 	max_uses = 220,
 	material = "default:bronze_ingot"
 })
 
 farming.register_hoe(":farming:hoe_mese", {
-	description = "Mese Hoe",
+	description = "Zappa di mese",
 	inventory_image = "farming_tool_mesehoe.png",
 	max_uses = 350,
 	material = "default:mese_crystal"
 })
 
 farming.register_hoe(":farming:hoe_diamond", {
-	description = "Diamond Hoe",
+	description = "Zappa di diamante",
 	inventory_image = "farming_tool_diamondhoe.png",
 	max_uses = 500,
 	material = "default:diamond"
