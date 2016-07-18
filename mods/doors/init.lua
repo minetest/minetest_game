3709fcff20fb90822aa5bcbdd0c950fda0f56fdf
Thomas--S <Thomas--S@users.noreply.github.com>
2016-07-18 07:14:01 +0200

    Make trapdoor textures work as expected.

--- mods/doors/init.lua
+++ mods/doors/init.lua
@@ -600,8 +600,10 @@ function doors.register_trapdoor(name, def)
 		type = "fixed",
 		fixed = {-0.5, -0.5, -0.5, 0.5, -6/16, 0.5}
 	}
-	def_closed.tiles = {def.tile_front, def.tile_front, def.tile_side, def.tile_side,
-		def.tile_side, def.tile_side}
+	def_closed.tiles = {def.tile_front,
+			def.tile_front .. '^[transformFY',
+			def.tile_side, def.tile_side,
+			def.tile_side, def.tile_side}
 
 	def_opened.node_box = {
 		type = "fixed",
@@ -614,7 +616,8 @@ function doors.register_trapdoor(name, def)
 	def_opened.tiles = {def.tile_side, def.tile_side,
 			def.tile_side .. '^[transform3',
 			def.tile_side .. '^[transform1',
-			def.tile_front, def.tile_front}
+			def.tile_front .. '^[transform46',
+			def.tile_front .. '^[transform6'}
 
 	def_opened.drop = name_closed
 	def_opened.groups.not_in_creative_inventory = 1
