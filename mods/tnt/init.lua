364feb126115552f769e819fee09f363a4b8e38d
tenplus1 <tenplus1@users.noreply.github.com>
2016-08-03 15:14:44 +0100

    simplified code
    simplified code

--- mods/tnt/init.lua
+++ mods/tnt/init.lua
@@ -47,10 +47,7 @@ end
 local function eject_drops(drops, pos, radius)
 	local drop_pos = vector.new(pos)
 	for _, item in pairs(drops) do
-		local count = item:get_count()
-		if count > 99 then
-			count = 99 -- limit overly large stacks
-		end
+		local count = math.min(item:get_count(), 99)
 		while count > 0 do
 			local take = math.max(1,math.min(radius * radius,
 					count,
