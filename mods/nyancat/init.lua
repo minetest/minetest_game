bc89faa1b28e5856e5203e3c2725aad072defa14
paramat <mat.gregory@virginmedia.com>
2016-11-10 03:52:03 +0000

    Nyancat: Make nyancat and rainbow light sources

--- mods/nyancat/init.lua
+++ mods/nyancat/init.lua
@@ -2,6 +2,8 @@ minetest.register_node("nyancat:nyancat", {
 	description = "Nyan Cat",
 	tiles = {"nyancat_side.png", "nyancat_side.png", "nyancat_side.png",
 		"nyancat_side.png", "nyancat_back.png", "nyancat_front.png"},
+	paramtype = "light",
+	light_source = default.LIGHT_MAX,
 	paramtype2 = "facedir",
 	groups = {cracky = 2},
 	is_ground_content = false,
@@ -16,6 +18,8 @@ minetest.register_node("nyancat:nyancat_rainbow", {
 		"nyancat_rainbow.png^[transformR90",
 		"nyancat_rainbow.png"
 	},
+	paramtype = "light",
+	light_source = default.LIGHT_MAX,
 	paramtype2 = "facedir",
 	groups = {cracky = 2},
 	is_ground_content = false,
