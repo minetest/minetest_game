b06368adf2c5e5d00fa2e34fb914e6bc67dc82cf
rubenwardy <rubenwardy@gmail.com>
2017-01-27 19:48:27 +0000

    Fix node drops not being added to inventory when not in creative mode

--- mods/creative/init.lua
+++ mods/creative/init.lua
@@ -42,10 +42,13 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 end)
 
 -- Don't pick up if the item is already in the inventory
+local old_handle_node_drops = minetest.handle_node_drops
 function minetest.handle_node_drops(pos, drops, digger)
-	if not digger or not digger:is_player() or
-			not creative.is_enabled_for(digger:get_player_name()) then
+	if not digger or not digger:is_player() then
 		return
+	end
+	if not creative.is_enabled_for(digger:get_player_name()) then
+		return old_handle_node_drops(pos, drops, digger)
 	end
 	local inv = digger:get_inventory()
 	if inv then
