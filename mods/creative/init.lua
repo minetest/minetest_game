007186e11b7928667009b56bfb5971a10029e100
zaoqi <zaoqiff@gmail.com>
2017-02-10 22:01:33 +0800

    恢复mods/creative/init.lua

--- mods/creative/init.lua
+++ mods/creative/init.lua
@@ -8,33 +8,35 @@ end
 
 dofile(minetest.get_modpath("creative") .. "/inventory.lua")
 
--- Dig time is modified according to difference (leveldiff) between tool
--- 'maxlevel' and node 'level'. Digtime is divided by the larger of
--- leveldiff and 1.
--- To speed up digging in creative, hand 'maxlevel' and 'digtime' have been
--- increased such that nodes of differing levels have an insignificant
--- effect on digtime.
-local digtime = 42
-local caps = {times = {digtime, digtime, digtime}, uses = 0, maxlevel = 256}
+if minetest.setting_getbool("creative_mode") then
+	-- Dig time is modified according to difference (leveldiff) between tool
+	-- 'maxlevel' and node 'level'. Digtime is divided by the larger of
+	-- leveldiff and 1.
+	-- To speed up digging in creative, hand 'maxlevel' and 'digtime' have been
+	-- increased such that nodes of differing levels have an insignificant
+	-- effect on digtime.
+	local digtime = 42
+	local caps = {times = {digtime, digtime, digtime}, uses = 0, maxlevel = 256}
 
-minetest.register_item(":", {
-	type = "none",
-	wield_image = "wieldhand.png",
-	wield_scale = {x = 1, y = 1, z = 2.5},
-	range = 10,
-	tool_capabilities = {
-		full_punch_interval = 0.5,
-		max_drop_level = 3,
-		groupcaps = {
-			crumbly = caps,
-			cracky  = caps,
-			snappy  = caps,
-			choppy  = caps,
-			oddly_breakable_by_hand = caps,
-		},
-		damage_groups = {fleshy = 10},
-	}
-})
+	minetest.register_item(":", {
+		type = "none",
+		wield_image = "wieldhand.png",
+		wield_scale = {x = 1, y = 1, z = 2.5},
+		range = 10,
+		tool_capabilities = {
+			full_punch_interval = 0.5,
+			max_drop_level = 3,
+			groupcaps = {
+				crumbly = caps,
+				cracky  = caps,
+				snappy  = caps,
+				choppy  = caps,
+				oddly_breakable_by_hand = caps,
+			},
+			damage_groups = {fleshy = 10},
+		}
+	})
+end
 
 -- Unlimited node placement
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack)
