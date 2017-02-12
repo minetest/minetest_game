78c632ebd4858c67b850817df5e5a8c4ae4811a1
Auke Kok <sofar@foo-projects.org>
2017-02-11 16:23:43 -0800

    Allow interaction with nodes while wielding these items.
    - beds
    - boats
    - carts
    - key/skeleton key
    - seeds
    
    All these had on_place handlers that did not allow nodes with
    an on_rightclick() handler to be used first (if not using
    sneak). This code is taken from the torches mod and applied
    everywhere.
    
    This allows all these items to e.g. be inserted into the `frame`
    mod's item frames.

--- mods/default/tools.lua
+++ mods/default/tools.lua
@@ -384,12 +384,21 @@ minetest.register_tool("default:skeleton_key", {
 	inventory_image = "default_key_skeleton.png",
 	groups = {key = 1},
 	on_place = function(itemstack, placer, pointed_thing)
+		local under = pointed_thing.under
+		local node = minetest.get_node(under)
+		local def = minetest.registered_nodes[node.name]
+		if def and def.on_rightclick and
+				not (placer and placer:get_player_control().sneak) then
+			return def.on_rightclick(under, node, placer, itemstack,
+				pointed_thing) or itemstack
+		end
+
 		if pointed_thing.type ~= "node" then
 			return itemstack
 		end
 
 		local pos = pointed_thing.under
-		local node = minetest.get_node(pos)
+		node = minetest.get_node(pos)
 
 		if not node then
 			return itemstack
@@ -427,12 +436,20 @@ minetest.register_tool("default:key", {
 	groups = {key = 1, not_in_creative_inventory = 1},
 	stack_max = 1,
 	on_place = function(itemstack, placer, pointed_thing)
+		local under = pointed_thing.under
+		local node = minetest.get_node(under)
+		local def = minetest.registered_nodes[node.name]
+		if def and def.on_rightclick and
+				not (placer and placer:get_player_control().sneak) then
+			return def.on_rightclick(under, node, placer, itemstack,
+				pointed_thing) or itemstack
+		end
 		if pointed_thing.type ~= "node" then
 			return itemstack
 		end
 
 		local pos = pointed_thing.under
-		local node = minetest.get_node(pos)
+		node = minetest.get_node(pos)
 
 		if not node or node.name == "ignore" then
 			return itemstack
