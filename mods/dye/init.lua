a9ba042bd362fab34590c68767718415d3fdc6d0
Trey Tomes <trey.tomes@gmail.com>
2017-02-13 09:14:38 -0600

    Generate dye items dynamically from the dye.dyes table.

--- mods/dye/init.lua
+++ mods/dye/init.lua
@@ -20,13 +20,104 @@ dye.dyes = {
 	pink		= { name = "Pink", html = "#FF7FFF" },
 }
 
--- Define items
+----
+-- Helper functions.
+----
+
+--[[
+	Convert an html color in the format of #RRGGBB into red, green, and blue values on the range of [0..255].
+]]
+function hex2rgb(hex)
+    hex = hex:gsub("#","")
+    return tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6))
+end
+
+function rgb2hex(r, g, b)
+	return string.format("#%02X%02X%02X", r, g, b)
+end
+
+
+--[[
+	Convert red, green, and blue values from the range of [0..255]
+	into hue, saturation, and value/lightness values on the range of...?
+]]
+function rgbToHsv(red, green, blue)
+	local r = red / 255
+	local g = green / 255
+	local b = blue / 255
+	local max = math.max(r, g, b)
+	local min = math.min(r, g, b)
+	local h, s, v = max, max, max
+
+	local d = max - min;
+	if max == 0 then
+		s = 0
+	else
+		s = d / max
+	end
+
+	if(max == min) then
+		h = 0; -- achromatic
+	else
+		if   max == r then
+			if g < b then
+				h = (g - b) / d + 6
+			else
+				h = (g - b) / d + 0
+			end
+		elseif max == g then
+			h = (b - r) / d + 2
+		elseif max == b then
+			h = (r - g) / d + 4
+		end
+		h = h / 6;
+	end
+	return h, s, v
+end
+
+--[[
+	Convert the hue, saturation, and value/lightness values output from rgbToHsv
+	back into red, green, and blue values on the range of [0..255].
+]]
+function hsvToRgb(h, s, v)
+	local i = math.floor(h * 6);
+	local f = h * 6 - i;
+	local p = v * (1 - s);
+	local q = v * (1 - f * s);
+	local t = v * (1 - (1 - f) * s);
+
+	local r, g, b;
+	local switch = i % 6
+	if     switch == 0 then r, g, b = v, t, p
+	elseif switch == 1 then r, g, b = q, v, p
+	elseif switch == 2 then r, g, b = p, v, t
+	elseif switch == 3 then r, g, b = p, q, v
+	elseif switch == 4 then r, g, b = t, p, v
+	elseif switch == 5 then r, g, b = v, p, q end
+
+	return math.floor(r * 255 + 0.5), math.floor(g * 255 + 0.5), math.floor(b * 255 + 0.5)
+end
+
+function darken(hexColor)
+	local r, g, b = hex2rgb(hexColor)
+	local h, s, v = rgbToHsv(r, g, b)
+	v = v * 0.75
+	r, g, b = hsvToRgb(h, s, v)
+	return rgb2hex(r, g, b)
+end
+
+----
+-- Define items.
+----
 
 for key, value in pairs(dye.dyes) do
 	local itemName = "dye:"..key
 
+	minetest.log("info", "Darkened dye: "..value.html.." to: "..darken(value.html))
+
 	minetest.register_craftitem(itemName, {
-		inventory_image = "dye_"..key..".png",
+		--inventory_image = "dye_"..key..".png",
+		inventory_image = "(dye_base.png^[colorize:"..darken(value.html)..")^(dye_highlight.png^[colorize:"..value.html ..")",
 		description = value.name,
 		groups = "color_"..key
 	})
