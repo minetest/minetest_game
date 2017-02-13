0a16acbf34a82deba0fb265e9630d90b2d3fcf92
Trey Tomes <trey.tomes@gmail.com>
2017-02-13 17:29:17 -0600

    Rewrote according to code review.

--- mods/dye/init.lua
+++ mods/dye/init.lua
@@ -3,45 +3,41 @@ dye = {}
 -- Make dye names and descriptions available globally.
 -- Other mods can use these for looping through available colors.
 dye.dyes = {
-	white		= { name = "White", html = "#FFFFFF" },
-	grey		= { name = "Grey", html = "#7F7F7F" },
-	dark_grey	= { name = "Dark Grey", html = "#3F3F3F" },
-	black		= { name = "Black", html = "#000000" },
-	violet		= { name = "Violet", html = "#8000FF" },
-	blue		= { name = "Blue", html = "#0000FF" },
-	cyan		= { name = "Cyan", html = "#00FFFF" },
-	dark_green	= { name = "Dark Green", html = "#007F00" },
-	green		= { name = "Green", html = "#00FF00" },
-	yellow		= { name = "Yellow", html = "#FFFF00" },
-	brown		= { name = "Brown", html = "#854C30" },
-	orange		= { name = "Orange", html = "#FF7F00" },
-	red			= { name = "Red", html = "#FF0000" },
-	magenta		= { name = "Magenta", html = "#FF00FF" },
-	pink		= { name = "Pink", html = "#FF7FFF" },
+	white      = { name = "White", html = "#FFFFFF" },
+	grey       = { name = "Grey", html = "#7F7F7F" },
+	dark_grey  = { name = "Dark Grey", html = "#3F3F3F" },
+	black      = { name = "Black", html = "#000000" },
+	violet     = { name = "Violet", html = "#8000FF" },
+	blue       = { name = "Blue", html = "#0000FF" },
+	cyan       = { name = "Cyan", html = "#00FFFF" },
+	dark_green = { name = "Dark Green", html = "#007F00" },
+	green      = { name = "Green", html = "#00FF00" },
+	yellow     = { name = "Yellow", html = "#FFFF00" },
+	brown      = { name = "Brown", html = "#854C30" },
+	orange	   = { name = "Orange", html = "#FF7F00" },
+	red        = { name = "Red", html = "#FF0000" },
+	magenta    = { name = "Magenta", html = "#FF00FF" },
+	pink       = { name = "Pink", html = "#FF7FFF" },
 }
 
 ----
 -- Helper functions.
 ----
 
---[[
-	Convert an html color in the format of #RRGGBB into red, green, and blue values on the range of [0..255].
-]]
-function hex2rgb(hex)
-    hex = hex:gsub("#","")
-    return tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6))
+-- Convert an html color in the format of #RRGGBB into red, green, and blue values on the range of [0..255].
+local function hex_to_rgb(html_color)
+	html_color = html_color:gsub("#", "")
+	return tonumber("0x"..html_color:sub(1, 2)), tonumber("0x"..html_color:sub(3, 4)), tonumber("0x"..html_color:sub(5, 6))
 end
 
-function rgb2hex(r, g, b)
-	return string.format("#%02X%02X%02X", r, g, b)
+-- Convert the red, green, and blue color components back into an html color string.
+local function rgb_to_hex(red, green, blue)
+	return string.format("#%02X%02X%02X", red, green, blue)
 end
 
 
---[[
-	Convert red, green, and blue values from the range of [0..255]
-	into hue, saturation, and value/lightness values on the range of...?
-]]
-function rgbToHsv(red, green, blue)
+-- Convert red, green, and blue values from the range of [0..255] into hue, saturation, and value/lightness values on the range of...?
+local function rgb_to_hsv(red, green, blue)
 	local r = red / 255
 	local g = green / 255
 	local b = blue / 255
@@ -50,16 +46,14 @@ function rgbToHsv(red, green, blue)
 	local h, s, v = max, max, max
 
 	local d = max - min;
-	if max == 0 then
-		s = 0
-	else
+	if max ~= 0 then
 		s = d / max
 	end
 
-	if(max == min) then
-		h = 0; -- achromatic
+	if max == min then
+		h = 0 -- achromatic
 	else
-		if   max == r then
+		if max == r then
 			if g < b then
 				h = (g - b) / d + 6
 			else
@@ -70,40 +64,44 @@ function rgbToHsv(red, green, blue)
 		elseif max == b then
 			h = (r - g) / d + 4
 		end
-		h = h / 6;
+		h = h / 6
 	end
 	return h, s, v
 end
 
 --[[
-	Convert the hue, saturation, and value/lightness values output from rgbToHsv
+	Convert the hue, saturation, and value/lightness values output from rgb_to_hsv
 	back into red, green, and blue values on the range of [0..255].
 ]]
-function hsvToRgb(h, s, v)
-	local i = math.floor(h * 6);
-	local f = h * 6 - i;
-	local p = v * (1 - s);
-	local q = v * (1 - f * s);
-	local t = v * (1 - (1 - f) * s);
+local function hsv_to_rgb(hue, saturation, value)
+	local i = math.floor(hue * 6)
+	local f = hue * 6 - i
+	local p = value * (1 - saturation)
+	local q = value * (1 - f * saturation)
+	local t = value * (1 - (1 - f) * saturation)
 
-	local r, g, b;
-	local switch = i % 6
-	if     switch == 0 then r, g, b = v, t, p
-	elseif switch == 1 then r, g, b = q, v, p
-	elseif switch == 2 then r, g, b = p, v, t
-	elseif switch == 3 then r, g, b = p, q, v
-	elseif switch == 4 then r, g, b = t, p, v
-	elseif switch == 5 then r, g, b = v, p, q end
+	local red, green, blue
 
-	return math.floor(r * 255 + 0.5), math.floor(g * 255 + 0.5), math.floor(b * 255 + 0.5)
+	local cases = {
+		[0] = function() red, green, blue = value, t, p end,
+		[1] = function() red, green, blue = q, value, p end,
+		[2] = function() red, green, blue = p, value, t end,
+		[3] = function() red, green, blue = p, q, value end,
+		[4] = function() red, green, blue = t, p, value end,
+		[5] = function() red, green, blue = value, p, q end
+	}
+	cases[i % 6]()
+
+	return math.floor(red * 255 + 0.5), math.floor(green * 255 + 0.5), math.floor(blue * 255 + 0.5)
 end
 
-function darken(hexColor)
-	local r, g, b = hex2rgb(hexColor)
-	local h, s, v = rgbToHsv(r, g, b)
-	v = v * 0.75
-	r, g, b = hsvToRgb(h, s, v)
-	return rgb2hex(r, g, b)
+-- Make the input html color darker by the given percent.
+local function darken_color(html_color, percent)
+	local r, g, b = hex_to_rgb(html_color)
+	local h, s, v = rgb_to_hsv(r, g, b)
+	v = v * (1 - percent)
+	r, g, b = hsv_to_rgb(h, s, v)
+	return rgb_to_hex(r, g, b)
 end
 
 ----
@@ -111,26 +109,21 @@ end
 ----
 
 for key, value in pairs(dye.dyes) do
-	local itemName = "dye:"..key
+	local item_name = "dye:"..key
 
-	minetest.log("info", "Darkened dye: "..value.html.." to: "..darken(value.html))
-
-	minetest.register_craftitem(itemName, {
-		--inventory_image = "dye_"..key..".png",
-		inventory_image = "(dye_base.png^[colorize:"..darken(value.html)..")^(dye_highlight.png^[colorize:"..value.html ..")",
-		description = value.name,
+	minetest.register_craftitem(item_name, {
+		inventory_image = "(dye_base.png^[colorize:"..darken_color(value.html, 0.25)..")^(dye_highlight.png^[colorize:"..value.html ..")",
+		description = value.name.." Dye",
 		groups = "color_"..key
 	})
 	minetest.register_craft({
 		type = "shapeless",
-		output = itemName .. " 4",
+		output = item_name .. " 4",
 		recipe = {"group:flower,color_"..key},
 	})
 end
 
 -- Manually add coal->black dye.
--- TODO: Is this still desirable?
-
 minetest.register_craft({
 	type = "shapeless",
 	output = "dye:black 4",
