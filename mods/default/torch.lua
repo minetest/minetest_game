1ec31d84727a3403ca357d704377f038e214f94d
paramat <mat.gregory@virginmedia.com>
2017-02-16 19:17:01 +0000

    Torches: Reduce light source level from 13 to 12
    As part of the original plan for a new lightcurve.
    
    With the old lightcurve lights were so dim all light sources had light
    source level 13 or 14 to compensate, resulting in almost no difference
    between torchlight and a maximum brightness light.
    
    The new lightcurve makes all light sources effectively much brighter by
    spreading visually-bright light further, torches are now slightly too
    bright. So now we can reduce the light source level of torches while
    actually making them effectively brighter than with the old lightcurve.
    This also creates a desirable difference between torchlight and a
    maximum-brightness light source.

--- mods/default/torch.lua
+++ mods/default/torch.lua
@@ -50,7 +50,7 @@ minetest.register_node("default:torch", {
 	sunlight_propagates = true,
 	walkable = false,
 	liquids_pointable = false,
-	light_source = 13,
+	light_source = 12,
 	groups = {choppy=2, dig_immediate=3, flammable=1, attached_node=1, torch=1},
 	drop = "default:torch",
 	selection_box = {
@@ -97,7 +97,7 @@ minetest.register_node("default:torch_wall", {
 	paramtype2 = "wallmounted",
 	sunlight_propagates = true,
 	walkable = false,
-	light_source = 13,
+	light_source = 12,
 	groups = {choppy=2, dig_immediate=3, flammable=1, not_in_creative_inventory=1, attached_node=1, torch=1},
 	drop = "default:torch",
 	selection_box = {
@@ -118,7 +118,7 @@ minetest.register_node("default:torch_ceiling", {
 	paramtype2 = "wallmounted",
 	sunlight_propagates = true,
 	walkable = false,
-	light_source = 13,
+	light_source = 12,
 	groups = {choppy=2, dig_immediate=3, flammable=1, not_in_creative_inventory=1, attached_node=1, torch=1},
 	drop = "default:torch",
 	selection_box = {
@@ -144,4 +144,3 @@ minetest.register_lbm({
 		end
 	end
 })
-
