minetest/minetest_game

commit b847888cb7b79787f424dec2c2ee1728cc2c0872

doors: add groups to the door craftitem

mods/doors/init.lua
@@ -254,6 +254,7 @@ function doors.register(name, def)
 	minetest.register_craftitem(":" .. name, {
 		description = def.description,
 		inventory_image = def.inventory_image,
+		groups = def.groups,
 
 		on_place = function(itemstack, placer, pointed_thing)
 			local pos

commit 3a7cde8dbe4b249b67070e51722a064c2d1dd9e7

doors: Fix craftitem not listed in inventory Fixes bug caused by b847888

mods/doors/init.lua
@@ -254,7 +254,7 @@ function doors.register(name, def)
 	minetest.register_craftitem(":" .. name, {
 		description = def.description,
 		inventory_image = def.inventory_image,
-		groups = def.groups,
+		groups = table.copy(def.groups),
 
 		on_place = function(itemstack, placer, pointed_thing)
 			local pos