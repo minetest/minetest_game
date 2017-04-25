2917562728927eda925d3e9ba17bd3cf945dae2a
rubenwardy <rubenwardy@gmail.com>
2017-04-26 00:15:15 +0100

    Fix owner not being checked on write to book

--- mods/default/craftitems.lua
+++ mods/default/craftitems.lua
@@ -12,6 +12,7 @@ minetest.register_craftitem("default:paper", {
 	groups = {flammable = 3},
 })
 
+
 local lpp = 14 -- Lines per book's page
 local function book_on_use(itemstack, user)
 	local player_name = user:get_player_name()
@@ -91,6 +92,10 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 			end
 		else
 			data = stack:get_meta():to_table().fields
+		end
+
+		if data and data.owner and data.owner ~= player:get_player_name() then
+			return
 		end
 
 		if not data then data = {} end
