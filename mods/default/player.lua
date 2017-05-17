fee8600eb94bb0004c9e7546b1e811109d19fde2
sfan5 <sfan5@live.de>
2017-05-17 20:56:39 +0200

    Remove set_inventory_formspec in default, remove delay in sfinv

--- mods/default/player.lua
+++ mods/default/player.lua
@@ -95,11 +95,6 @@ minetest.register_on_joinplayer(function(player)
 	default.player_set_model(player, "character.b3d")
 	player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
 
-	-- set GUI
-	if not (creative and creative.is_enabled_for
-			and creative.is_enabled_for(player:get_player_name())) then
-		player:set_inventory_formspec(default.gui_survival_form)
-	end
 	player:hud_set_hotbar_image("gui_hotbar.png")
 	player:hud_set_hotbar_selected_image("gui_hotbar_selected.png")
 end)
