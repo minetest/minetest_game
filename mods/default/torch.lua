547feef906ea623c994f840b08a5379834a915e3
Johannes Fritz <Mr.rar1998@gmail.com>
2017-06-10 12:53:16 -0500

    Add files via upload

--- mods/default/torch.lua
+++ mods/default/torch.lua
@@ -43,6 +43,7 @@ minetest.register_node("default:torch", {
 	wield_image = "default_torch_on_floor.png",
 	tiles = {{
 		    name = "default_torch_on_floor_animated.png",
+		    backface_culling = true,
 		    animation = {type = "vertical_frames", aspect_w = 16, aspect_h = 16, length = 3.3}
 	}},
 	paramtype = "light",
@@ -91,6 +92,7 @@ minetest.register_node("default:torch_wall", {
 	mesh = "torch_wall.obj",
 	tiles = {{
 		    name = "default_torch_on_floor_animated.png",
+		    backface_culling = true,
 		    animation = {type = "vertical_frames", aspect_w = 16, aspect_h = 16, length = 3.3}
 	}},
 	paramtype = "light",
@@ -112,6 +114,7 @@ minetest.register_node("default:torch_ceiling", {
 	mesh = "torch_ceiling.obj",
 	tiles = {{
 		    name = "default_torch_on_floor_animated.png",
+		    backface_culling = true,
 		    animation = {type = "vertical_frames", aspect_w = 16, aspect_h = 16, length = 3.3}
 	}},
 	paramtype = "light",
