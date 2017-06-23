2c666891a8171a33a305246c991edd458206cab0
F.K.Z <farooghkarimizadeh@gmail.com>
2017-06-23 07:04:30 +0430

    Furnace formspec: Replace deprecated 'current_name' with 'context'

--- mods/default/furnace.lua
+++ mods/default/furnace.lua
@@ -9,20 +9,20 @@ local function active_formspec(fuel_percent, item_percent)
 		default.gui_bg..
 		default.gui_bg_img..
 		default.gui_slots..
-		"list[current_name;src;2.75,0.5;1,1;]"..
-		"list[current_name;fuel;2.75,2.5;1,1;]"..
+		"list[context;src;2.75,0.5;1,1;]"..
+		"list[context;fuel;2.75,2.5;1,1;]"..
 		"image[2.75,1.5;1,1;default_furnace_fire_bg.png^[lowpart:"..
 		(100-fuel_percent)..":default_furnace_fire_fg.png]"..
 		"image[3.75,1.5;1,1;gui_furnace_arrow_bg.png^[lowpart:"..
 		(item_percent)..":gui_furnace_arrow_fg.png^[transformR270]"..
-		"list[current_name;dst;4.75,0.96;2,2;]"..
+		"list[context;dst;4.75,0.96;2,2;]"..
 		"list[current_player;main;0,4.25;8,1;]"..
 		"list[current_player;main;0,5.5;8,3;8]"..
-		"listring[current_name;dst]"..
+		"listring[context;dst]"..
 		"listring[current_player;main]"..
-		"listring[current_name;src]"..
+		"listring[context;src]"..
 		"listring[current_player;main]"..
-		"listring[current_name;fuel]"..
+		"listring[context;fuel]"..
 		"listring[current_player;main]"..
 		default.get_hotbar_bg(0, 4.25)
 	return formspec
@@ -33,18 +33,18 @@ local inactive_formspec =
 	default.gui_bg..
 	default.gui_bg_img..
 	default.gui_slots..
-	"list[current_name;src;2.75,0.5;1,1;]"..
-	"list[current_name;fuel;2.75,2.5;1,1;]"..
+	"list[context;src;2.75,0.5;1,1;]"..
+	"list[context;fuel;2.75,2.5;1,1;]"..
 	"image[2.75,1.5;1,1;default_furnace_fire_bg.png]"..
 	"image[3.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
-	"list[current_name;dst;4.75,0.96;2,2;]"..
+	"list[context;dst;4.75,0.96;2,2;]"..
 	"list[current_player;main;0,4.25;8,1;]"..
 	"list[current_player;main;0,5.5;8,3;8]"..
-	"listring[current_name;dst]"..
+	"listring[context;dst]"..
 	"listring[current_player;main]"..
-	"listring[current_name;src]"..
+	"listring[context;src]"..
 	"listring[current_player;main]"..
-	"listring[current_name;fuel]"..
+	"listring[context;fuel]"..
 	"listring[current_player;main]"..
 	default.get_hotbar_bg(0, 4.25)
 
