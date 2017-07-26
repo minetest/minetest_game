5d19fd6923b46f812fa04f09644f725ea8823b81
rubenwardy <rubenwardy@gmail.com>
2017-07-26 19:38:11 +0100

    Separate player code into new mod

--- mods/default/legacy.lua
+++ mods/default/legacy.lua
@@ -23,3 +23,14 @@ LIGHT_MAX = default.LIGHT_MAX
 
 -- Formspecs
 default.gui_suvival_form = default.gui_survival_form
+
+-- Players
+if minetest.get_modpath("player_api") then
+	default.registered_player_models = player_api.registered_models
+	default.player_register_model    = player_api.register_model
+	default.player_attached          = player_api.player_attached
+	default.player_get_animation     = player_api.get_animation
+	default.player_set_model         = player_api.set_model
+	default.player_set_textures      = player_api.set_textures
+	default.player_set_animation     = player_api.set_animation
+end
