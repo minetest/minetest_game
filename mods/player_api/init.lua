77c94087211a9ca41dcfa5bc2d6732f1cdf33425
paramat <paramat@users.noreply.github.com>
2017-08-07 04:27:19 +0100

    Player API: Integrate settable player step height
    Split some long lines.
    Some nearby code cleanup.

--- mods/player_api/init.lua
+++ mods/player_api/init.lua
@@ -6,22 +6,28 @@ player_api.register_model("character.b3d", {
 	textures = {"character.png", },
 	animations = {
 		-- Standard animations.
-		stand     = { x=  0, y= 79, },
-		lay       = { x=162, y=166, },
-		walk      = { x=168, y=187, },
-		mine      = { x=189, y=198, },
-		walk_mine = { x=200, y=219, },
-		sit       = { x= 81, y=160, },
+		stand     = {x = 0,   y = 79},
+		lay       = {x = 162, y = 166},
+		walk      = {x = 168, y = 187},
+		mine      = {x = 189, y = 198},
+		walk_mine = {x = 200, y = 219},
+		sit       = {x = 81,  y = 160},
 	},
 	collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.77, 0.3},
+	stepheight = 0.6,
 })
 
 -- Update appearance when the player joins
 minetest.register_on_joinplayer(function(player)
 	player_api.player_attached[player:get_player_name()] = false
 	player_api.set_model(player, "character.b3d")
-	player:set_local_animation({x=0, y=79}, {x=168, y=187}, {x=189, y=198}, {x=200, y=219}, 30)
-
+	player:set_local_animation(
+		{x = 0,   y = 79},
+		{x = 168, y = 187},
+		{x = 189, y = 198},
+		{x = 200, y = 219},
+		30
+	)
 	player:hud_set_hotbar_image("gui_hotbar.png")
 	player:hud_set_hotbar_selected_image("gui_hotbar_selected.png")
 end)
