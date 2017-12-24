faa088e2c5a9ddc4e73f2353c1217d105c78fda5
MarkuBu <markus.burrer@gmail.com>
2017-12-24 22:36:41 +0100

    Xpanes: Add optional 'use_texture_alpha' parameter to registration

--- mods/xpanes/init.lua
+++ mods/xpanes/init.lua
@@ -104,6 +104,7 @@ function xpanes.register_pane(name, def)
 		groups = flatgroups,
 		drop = "xpanes:" .. name .. "_flat",
 		sounds = def.sounds,
+		use_texture_alpha = def.use_texture_alpha or false,
 		node_box = {
 			type = "fixed",
 			fixed = {{-1/2, -1/2, -1/32, 1/2, 1/2, 1/32}},
@@ -128,6 +129,7 @@ function xpanes.register_pane(name, def)
 		groups = groups,
 		drop = "xpanes:" .. name .. "_flat",
 		sounds = def.sounds,
+		use_texture_alpha = def.use_texture_alpha or false,
 		node_box = {
 			type = "connected",
 			fixed = {{-1/32, -1/2, -1/32, 1/32, 1/2, 1/32}},
