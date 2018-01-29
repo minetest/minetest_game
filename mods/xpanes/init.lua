bfae6d7aef018c421bd4455919da642d492b8a46
TumeniNodes <tumeninodes@gmail.com>
2018-01-29 16:52:21 -0500

    add obsidian_pane
    adds an obsidian glass pane to xpanes

--- mods/xpanes/init.lua
+++ mods/xpanes/init.lua
@@ -160,6 +160,19 @@ xpanes.register_pane("pane", {
 	}
 })
 
+xpanes.register_pane("obsidian_pane", {
+	description = "Obsidian Glass Pane",
+	textures = {"default_obsidian_glass.png","xpanes_pane_half.png","xpanes_white.png"},
+	inventory_image = "default_obsidian_glass.png",
+	wield_image = "default_obsidian_glass.png",
+	sounds = default.node_sound_glass_defaults(),
+	groups = {snappy=2, cracky=3, oddly_breakable_by_hand=3},
+	recipe = {
+		{"default:obsidian_glass", "default:obsidian_glass", "default:obsidian_glass"},
+		{"default:obsidian_glass", "default:obsidian_glass", "default:obsidian_glass"}
+	}
+})
+
 xpanes.register_pane("bar", {
 	description = "Iron Bar",
 	textures = {"xpanes_bar.png","xpanes_bar.png","xpanes_bar_top.png"},
