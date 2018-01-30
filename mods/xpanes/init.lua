24ae9d2e5d32d5f9dbaeb6b84a0558f16cc55636
TumeniNodes <tumeninodes@gmail.com>
2018-01-30 15:30:19 -0500

    fix some many blunders
    restores use_texture_alpha = true
    removes using adding glass detail which already exists
    fixes wrong indentation

--- mods/xpanes/init.lua
+++ mods/xpanes/init.lua
@@ -104,6 +104,7 @@ function xpanes.register_pane(name, def)
 		groups = flatgroups,
 		drop = "xpanes:" .. name .. "_flat",
 		sounds = def.sounds,
+        use_texture_alpha = def.use_texture_alpha or false,
 		node_box = {
 			type = "fixed",
 			fixed = {{-1/2, -1/2, -1/32, 1/2, 1/2, 1/32}},
@@ -147,10 +148,9 @@ end
 
 xpanes.register_pane("pane", {
 	description = "Glass Pane",
-	textures = {"default_glass.png^default_glass_detail.png","xpanes_pane_half.png","xpanes_white.png"},
-    use_texture_alpha = true,
-	inventory_image = "default_glass.png^default_glass_detail.png",
-	wield_image = "default_glass.png^default_glass_detail.png",
+	textures = {"default_glass.png","xpanes_pane_half.png","xpanes_white.png"},
+    inventory_image = "default_glass.png",
+	wield_image = "default_glass.png",
 	sounds = default.node_sound_glass_defaults(),
 	groups = {snappy=2, cracky=3, oddly_breakable_by_hand=3},
 	recipe = {
