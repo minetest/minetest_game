30bc92adf720445e93507d5438eb84c08334b2c4
ezhh <owlecho@live.com>
2018-02-25 11:08:26 +0000

    Fix

--- mods/default/mapgen.lua
+++ mods/default/mapgen.lua
@@ -646,7 +646,7 @@ function default.register_ores()
 		clust_scarcity = 12 * 12 * 12,
 		clust_num_ores = 4,
 		clust_size     = 3,
-		y_max          = 31000,
+		y_max          = -128,
 		y_min          = -255,
 	})
 
