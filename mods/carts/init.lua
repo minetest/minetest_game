ecea5364f1bf0e1e6dd553c52c4122affd006c39
SmallJoker <SmallJoker@users.noreply.github.com>
2018-07-07 03:08:36 +0200

    Update carts from boost_cart
    Better pathfinder algorithm, allows tuning the lag spike compensation.
    Smoother movement (when it's laggy).
    Set the player animation to stand on attach.
    Remove driver when they leave.
    Only update velocity when it's necessary.

--- mods/carts/init.lua
+++ mods/carts/init.lua
@@ -7,6 +7,8 @@ carts.railparams = {}
 carts.speed_max = 7
 -- Set to -1 to disable punching the cart from inside (min = -1)
 carts.punch_speed_max = 5
+-- Maximal distance for the path correction (for dtime peaks)
+carts.path_distance_max = 3
 
 
 dofile(carts.modpath.."/functions.lua")
