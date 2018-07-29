c284e52963ee78afda8f12bbaf915c55df2eb3d1
Paramat <paramat@users.noreply.github.com>
2018-07-30 00:32:18 +0100

    Boats, carts mods: Use 'initial_properties' table

--- mods/carts/cart_entity.lua
+++ mods/carts/cart_entity.lua
@@ -1,10 +1,12 @@
 local cart_entity = {
-	physical = false, -- otherwise going uphill breaks
-	collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-	visual = "mesh",
-	mesh = "carts_cart.b3d",
-	visual_size = {x=1, y=1},
-	textures = {"carts_cart.png"},
+	initial_properties = {
+		physical = false, -- otherwise going uphill breaks
+		collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
+		visual = "mesh",
+		mesh = "carts_cart.b3d",
+		visual_size = {x=1, y=1},
+		textures = {"carts_cart.png"},
+	},
 
 	driver = nil,
 	punched = false, -- used to re-send velocity and position
