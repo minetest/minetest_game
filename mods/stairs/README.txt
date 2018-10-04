81223da64e097e1df28a126d9f1ff62b2f29868a
TumeniNodes <tumeninodes@gmail.com>
2018-10-04 16:31:43 -0400

    add glass and obsidian glass stairs

--- mods/stairs/README.txt
+++ mods/stairs/README.txt
@@ -7,3 +7,13 @@ Authors of source code
 Originally by Kahrl <kahrl@gmx.net> (LGPLv2.1+) and
 celeron55, Perttu Ahola <celeron55@gmail.com> (LGPLv2.1+)
 Various Minetest developers and contributors (LGPLv2.1+)
+
+Authors of media (textures)
+---------------------------
+
+Textures
+--------
+
+TumeniNodes (CC BY-SA 3.0):
+  default_glass_slab -- Derived from a texture by celeron55, Perttu Ahola <celeron55@gmail.com> (CC BY-SA 3.0)
+  default_obsidian_glass_slab -- Derived from a texture by PilzAdam (CC BY-SA 3.0)
--- mods/stairs/init.lua
+++ mods/stairs/init.lua
@@ -855,3 +855,23 @@ stairs.register_stair_and_slab(
 	default.node_sound_snow_defaults(),
 	true
 )
+
+stairs.register_stair_and_slab(
+	"glass",
+	"default:glass",
+	{cracky = 3, oddly_breakable_by_hand = 3},
+	{"default_glass.png", "default_glass.png", "stairs_glass_slab.png"},
+	"Glass Stair",
+	"Glass Slab",
+	default.node_sound_glass_defaults()
+)
+
+stairs.register_stair_and_slab(
+	"obsidian_glass",
+	"default:obsidian_glass",
+	{cracky = 3},
+	{"default_obsidian_glass.png", "default_obsidian_glass.png", "stairs_obsidian_glass_slab.png"},
+	"Obsidian Glass Stair",
+	"Obsidian Glass Slab",
+	default.node_sound_glass_defaults()
+)
