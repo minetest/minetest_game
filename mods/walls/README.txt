f8693a6a5b9943e3bab7a33062ab3ae9a0673c1a
TumeniNodes <tumeninodes@gmail.com>
2018-10-11 15:59:08 -0400

    add wall extension

--- mods/walls/README.txt
+++ mods/walls/README.txt
@@ -5,3 +5,5 @@ See license.txt for license information.
 Authors of source code
 ----------------------
 Auke Kok <sofar@foo-projects.org> (LGPLv2.1+)
+
+Shara Redcat (LGPLv2.1+)
--- mods/walls/init.lua
+++ mods/walls/init.lua
@@ -25,6 +25,55 @@ walls.register = function(wall_name, wall_desc, wall_texture_table, wall_mat, wa
 		walkable = true,
 		groups = { cracky = 3, wall = 1, stone = 2 },
 		sounds = wall_sounds,
+				after_place_node = function(pos, placer, itemstack, pointed_thing)
+			local pos_under = {x = pos.x, y = pos.y - 1, z = pos.z}
+			local pos_above = {x = pos.x, y = pos.y + 1, z = pos.z}
+			local node_under = string.gsub(minetest.get_node(pos_under).name, "_full$", "")
+			local node_above = string.gsub(minetest.get_node(pos_above).name, "_full$", "")
+
+			if minetest.get_item_group(node_under, "wall") == 1 then
+				minetest.set_node(pos_under, {name = node_under .. "_full"})
+			end
+			if minetest.get_item_group(node_above, "wall") == 1 then
+				minetest.set_node(pos, {name = wall_name .. "_full"})
+			end
+		end,
+		after_dig_node = function(pos, oldnode, oldmetadata, digger)
+			local pos_under = {x = pos.x, y = pos.y - 1, z = pos.z}
+			local node_under = string.gsub(minetest.get_node(pos_under).name, "_full$", "")
+			if minetest.get_item_group(node_under, "wall") == 1 and
+					digger and digger:is_player() then
+				minetest.set_node(pos_under, {name = node_under})
+			end
+		end,
+	})
+
+	minetest.register_node(wall_name .. "_full", {
+		description = wall_desc,
+		drawtype = "nodebox",
+		node_box = {
+			type = "connected",
+			fixed = {{-1/4, -1/2, -1/4, 1/4, 1/2, 1/4}},
+			connect_front = {{-3/16, -1/2, -1/2,  3/16, 1/2, -1/4}},
+			connect_left = {{-1/2, -1/2, -3/16, -1/4, 1/2,  3/16}},
+			connect_back = {{-3/16, -1/2,  1/4,  3/16, 1/2,  1/2}},
+			connect_right = {{ 1/4, -1/2, -3/16,  1/2, 1/2,  3/16}},
+		},
+		connects_to = {"group:wall", "group:stone", "group:fence"},
+		paramtype = "light",
+		is_ground_content = false,
+		tiles = wall_texture_table,
+		groups = {cracky = 3, wall = 1, stone = 2, not_in_creative_inventory = 1},
+		sounds = wall_sounds,
+		drop = wall_name,
+		after_dig_node = function(pos, oldnode, oldmetadata, digger)
+			local pos_under = {x = pos.x, y = pos.y - 1, z = pos.z}
+			local node_under = string.gsub(minetest.get_node(pos_under).name, "_full$", "")
+			if minetest.get_item_group(node_under, "wall") == 1 and
+					digger and digger:is_player() then
+				minetest.set_node(pos_under, {name = node_under})
+			end
+		end,
 	})
 
 	-- crafting recipe
