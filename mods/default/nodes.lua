18b1d7eb3e692da3e0e1225767dd03bf4e6ac8e5
paramat <paramat@users.noreply.github.com>
2018-12-31 04:45:10 +0000

    Use random range for seedling timer

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -1317,7 +1317,8 @@ minetest.register_node("default:large_cactus_seedling", {
 		-- 83 / 5 = 16.6 intervals = 16.6 * 12 = 199.2s.
 		-- Large cactus contains on average 14 cactus nodes.
 		-- 14 * 199.2 = 2788.8s.
-		minetest.get_node_timer(pos):start(2789)
+		-- Set random range to average to 2789s.
+		minetest.get_node_timer(pos):start(math.random(1859, 3719))
 	end,
 
 	on_timer = function(pos)
