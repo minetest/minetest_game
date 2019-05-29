9d308c1039ec32205c2d8ea5d60764a280140529
An0n3m0us <abdullaahiqbal@hotmail.com>
2019-05-29 11:31:40 +0100

    Add trapdoor model for default trapdoors only

--- mods/doors/init.lua
+++ mods/doors/init.lua
@@ -644,17 +644,24 @@ function doors.register_trapdoor(name, def)
 	local def_opened = table.copy(def)
 	local def_closed = table.copy(def)
 
-	def_closed.node_box = {
-		type = "fixed",
-		fixed = {
-                    {-0.5, -0.5, -0.5, 0.5, -6/16, -5/16},
-                    {-0.5, -0.5, 5/16, 0.5, -6/16, 0.5},
-                    {-0.5, -0.5, -5/16, -5/16, -6/16, 5/16},
-                    {5/16, -0.5, -5/16, 0.5, -6/16, 5/16},
-                    {-5/16, -0.5, -2/16, 5/16, -6/16, 2/16},
-                    {-2/16, -0.5, -5/16, 2/16, -6/16, 6/16}
-		}
-	}
+    if name == "doors:trapdoor" or name == "doors:trapdoor_steel" then
+	    def_closed.node_box = {
+	        type = "fixed",
+	        fixed = {
+                        {-0.5, -0.5, -0.5, 0.5, -6/16, -5/16},
+                        {-0.5, -0.5, 5/16, 0.5, -6/16, 0.5},
+                        {-0.5, -0.5, -5/16, -5/16, -6/16, 5/16},
+                        {5/16, -0.5, -5/16, 0.5, -6/16, 5/16},
+                        {-5/16, -0.5, -2/16, 5/16, -6/16, 2/16},
+                        {-2/16, -0.5, -5/16, 2/16, -6/16, 6/16}
+	        }
+        }
+    else
+        def_closed.node_box = {
+            type = "fixed",
+            fixed = {-0.5, -0.5, -0.5, 0.5, -6/16, 0.5}
+        }
+    end
 	def_closed.selection_box = {
 		type = "fixed",
 		fixed = {-0.5, -0.5, -0.5, 0.5, -6/16, 0.5}
@@ -668,17 +675,24 @@ function doors.register_trapdoor(name, def)
 		def.tile_side
 	}
 
-	def_opened.node_box = {
-		type = "fixed",
-		fixed = {
-                    {-0.5, -0.5, 6/16, -5/16, 0.5, 0.5},
-                    {5/16, -0.5, 6/16, 0.5, 0.5, 0.5},
-                    {-5/16, 5/16, 6/16, 5/16, 0.5, 0.5},
-                    {-5/16, -0.5, 6/16, 5/16, -5/16, 0.5},
-                    {-2/16, -6/16, 6/16, 2/16, 5/16, 0.5},
-                    {-5/16, -2/16, 6/16, 5/16, 2/16, 0.5}
-		}
-	}
+    if name == "doors:trapdoor" or name == "doors:trapdoor_steel" then
+	    def_opened.node_box = {
+	        type = "fixed",
+	        fixed = {
+                        {-0.5, -0.5, 6/16, -5/16, 0.5, 0.5},
+                        {5/16, -0.5, 6/16, 0.5, 0.5, 0.5},
+                        {-5/16, 5/16, 6/16, 5/16, 0.5, 0.5},
+                        {-5/16, -0.5, 6/16, 5/16, -5/16, 0.5},
+                        {-2/16, -6/16, 6/16, 2/16, 5/16, 0.5},
+                        {-5/16, -2/16, 6/16, 5/16, 2/16, 0.5}
+	        }
+        }
+    else
+        def_opened.node_box = {
+            type = "fixed",
+            fixed = {-0.5, -0.5, 6/16, 0.5, 0.5, 0.5}
+        }
+    end
 	def_opened.selection_box = {
 		type = "fixed",
 		fixed = {-0.5, -0.5, 6/16, 0.5, 0.5, 0.5}
