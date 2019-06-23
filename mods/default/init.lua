5f34731a01061bba368ba97f9121a463cb64847b
rubenwardy <rw@rubenwardy.com>
2019-06-23 03:06:29 +0100

    Use 9-slice background in formspec theme

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -12,7 +12,7 @@ default.LIGHT_MAX = 14
 minetest.register_on_joinplayer(function(player)
 	player:set_formspec_prepend([[
 			bgcolor[#080808BB;true]
-			background[5,5;1,1;gui_formbg.png;true]
+			background[5,5;1,1;gui_formbg.png;true;10]
 			listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF] ]])
 end)
 
