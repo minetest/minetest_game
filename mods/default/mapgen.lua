6716fc74ecec814d9e2e6b4134c820252711ded7
Paramat <paramat@users.noreply.github.com>
2019-07-30 03:55:17 +0100

    Add patches of bare 'dry dirt' to savanna
    Tune noise to appear roughly where long dry grass is least dense and shortest.

--- mods/default/mapgen.lua
+++ mods/default/mapgen.lua
@@ -1838,6 +1838,30 @@ end
 
 
 function default.register_decorations()
+	-- Savanna bare dirt patches.
+	-- Must come before all savanna decorations that are placed on dry grass.
+	-- Noise is similar to long dry grass noise, but scale inverted, to appear
+	-- where long dry grass is least dense and shortest.
+
+	minetest.register_decoration({
+		deco_type = "simple",
+		place_on = {"default:dry_dirt_with_dry_grass"},
+		sidelen = 4,
+		noise_params = {
+			offset = -1.5,
+			scale = -1.5,
+			spread = {x = 200, y = 200, z = 200},
+			seed = 329,
+			octaves = 4,
+			persist = 1.0
+		},
+		biomes = {"savanna"},
+		y_max = 31000,
+		y_min = 1,
+		decoration = "default:dry_dirt",
+		place_offset_y = -1,
+		flags = "force_placement",
+	})
 
 	-- Apple tree and log
 
