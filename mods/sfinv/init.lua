4eadf806349e9acfc817db8cdcdc85bc879c1558
Paramat <paramat@users.noreply.github.com>
2019-08-13 21:57:32 +0100

    Sfinv: Add and use a crafting arrow texture instead of using furnace arrow

--- mods/sfinv/init.lua
+++ mods/sfinv/init.lua
@@ -6,7 +6,7 @@ sfinv.register_page("sfinv:crafting", {
 		return sfinv.make_formspec(player, context, [[
 				list[current_player;craft;1.75,0.5;3,3;]
 				list[current_player;craftpreview;5.75,1.5;1,1;]
-				image[4.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]
+				image[4.75,1.5;1,1;sfinv_crafting_arrow.png]
 				listring[current_player;main]
 				listring[current_player;craft]
 			]], true)
