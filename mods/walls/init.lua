6b47c1400765aa16d22f10e9ca4558074d5be869
mbartlett21 <29034492+mbartlett21@users.noreply.github.com>
2019-09-03 16:44:23 +1000

    Tall walls

--- mods/walls/init.lua
+++ mods/walls/init.lua
@@ -1,5 +1,7 @@
 walls = {}
 
+local fence_collision_extra = minetest.settings:get_bool("enable_fence_tall") and 3/8 or 0
+
 walls.register = function(wall_name, wall_desc, wall_texture_table, wall_mat, wall_sounds)
 	--make wall_texture_table paramenter backwards compatible for mods passing single texture
 	if type(wall_texture_table) ~= "table" then
@@ -11,12 +13,23 @@ walls.register = function(wall_name, wall_desc, wall_texture_table, wall_mat, wa
 		drawtype = "nodebox",
 		node_box = {
 			type = "connected",
-			fixed = {{-1/4, -1/2, -1/4, 1/4, 1/2, 1/4}},
+			fixed = {-1/4, -1/2, -1/4, 1/4, 1/2, 1/4},
+			-- connect_top =
 			-- connect_bottom =
-			connect_front = {{-3/16, -1/2, -1/2,  3/16, 3/8, -1/4}},
-			connect_left = {{-1/2, -1/2, -3/16, -1/4, 3/8,  3/16}},
-			connect_back = {{-3/16, -1/2,  1/4,  3/16, 3/8,  1/2}},
-			connect_right = {{ 1/4, -1/2, -3/16,  1/2, 3/8,  3/16}},
+			connect_front = {-3/16, -1/2, -1/2,  3/16, 3/8, -1/4},
+			connect_left = {-1/2, -1/2, -3/16, -1/4, 3/8,  3/16},
+			connect_back = {-3/16, -1/2,  1/4,  3/16, 3/8,  1/2},
+			connect_right = { 1/4, -1/2, -3/16,  1/2, 3/8,  3/16},
+		},
+		collision_box = {
+			type = "connected",
+			fixed = {-1/4, -1/2, -1/4, 1/4, 1/2 + fence_collision_extra, 1/4},
+			-- connect_top =
+			-- connect_bottom =
+			connect_front = {-1/4,-1/2,-1/2,1/4,1/2 + fence_collision_extra,-1/4},
+			connect_left = {-1/2,-1/2,-1/4,-1/4,1/2 + fence_collision_extra,1/4},
+			connect_back = {-1/4,-1/2,1/4,1/4,1/2 + fence_collision_extra,1/2},
+			connect_right = {1/4,-1/2,-1/4,1/2,1/2 + fence_collision_extra,1/4},
 		},
 		connects_to = { "group:wall", "group:stone", "group:fence" },
 		paramtype = "light",
