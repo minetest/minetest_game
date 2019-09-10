0e1a23e48765ee1722aee325ce78cecaa8770814
sfan5 <sfan5@live.de>
2019-09-10 19:57:33 +0200

    bones

--- mods/bones/init.lua
+++ mods/bones/init.lua
@@ -269,7 +269,7 @@ minetest.register_on_dieplayer(function(player)
 	meta:set_string("owner", player_name)
 
 	if share_bones_time ~= 0 then
-		meta:set_string("infotext", S("@1's fresh bones.", player_name))
+		meta:set_string("infotext", S("@1's fresh bones", player_name))
 
 		if share_bones_time_early == 0 or not minetest.is_protected(pos, player_name) then
 			meta:set_int("time", 0)
@@ -279,6 +279,6 @@ minetest.register_on_dieplayer(function(player)
 
 		minetest.get_node_timer(pos):start(10)
 	else
-		meta:set_string("infotext", S("@1's bones.", player_name))
+		meta:set_string("infotext", S("@1's bones", player_name))
 	end
 end)
