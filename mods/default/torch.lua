ee0501b091b72b67a7d09f0187c46176ea28298e
An0n3m0us <an0n3m0us@hotmail.com>
2020-01-29 22:26:37 +0000

    Remove unrequired torch registry

--- mods/default/torch.lua
+++ mods/default/torch.lua
@@ -39,8 +39,12 @@ minetest.register_node("default:torch", {
 	selection_box = {
 		type = "wallmounted",
 		wall_bottom = {-1/8, -1/2, -1/8, 1/8, 2/16, 1/8},
+		wall_top = {-1/8, -2/16, -1/8, 1/8, 8/16, 1/8},
 	},
 	sounds = default.node_sound_wood_defaults(),
+	floodable = true,
+	on_flood = on_flood,
+
 	on_place = function(itemstack, placer, pointed_thing)
 		local under = pointed_thing.under
 		local node = minetest.get_node(under)
@@ -55,11 +59,9 @@ minetest.register_node("default:torch", {
 		local above = pointed_thing.above
 		local wdir = minetest.dir_to_wallmounted(vector.subtract(under, above))
 		local fakestack = itemstack
-		if wdir == 0 then
-			fakestack:set_name("default:torch_ceiling")
-		elseif wdir == 1 then
+		if wdir == 1 then
 			fakestack:set_name("default:torch")
-		else
+		elseif wdir ~= 0 then
 			fakestack:set_name("default:torch_wall")
 		end
 
@@ -67,9 +69,7 @@ minetest.register_node("default:torch", {
 		itemstack:set_name("default:torch")
 
 		return itemstack
-	end,
-	floodable = true,
-	on_flood = on_flood,
+	end
 })
 
 minetest.register_node("default:torch_wall", {
@@ -95,40 +95,14 @@ minetest.register_node("default:torch_wall", {
 	on_flood = on_flood,
 })
 
-minetest.register_node("default:torch_ceiling", {
-	drawtype = "mesh",
-	mesh = "torch_ceiling.obj",
-	tiles = {{
-		    name = "default_torch_on_floor_animated.png",
-		    animation = {type = "vertical_frames", aspect_w = 16, aspect_h = 16, length = 3.3}
-	}},
-	paramtype = "light",
-	paramtype2 = "wallmounted",
-	sunlight_propagates = true,
-	walkable = false,
-	light_source = 12,
-	groups = {choppy=2, dig_immediate=3, flammable=1, not_in_creative_inventory=1, attached_node=1, torch=1},
-	drop = "default:torch",
-	selection_box = {
-		type = "wallmounted",
-		wall_top = {-1/8, -1/16, -5/16, 1/8, 1/2, 1/8},
-	},
-	sounds = default.node_sound_wood_defaults(),
-	floodable = true,
-	on_flood = on_flood,
-})
-
 minetest.register_lbm({
 	name = "default:3dtorch",
 	nodenames = {"default:torch", "torches:floor", "torches:wall"},
 	action = function(pos, node)
-		if node.param2 == 0 then
-			minetest.set_node(pos, {name = "default:torch_ceiling",
-				param2 = node.param2})
-		elseif node.param2 == 1 then
+		if node.param2 == 1 then
 			minetest.set_node(pos, {name = "default:torch",
 				param2 = node.param2})
-		else
+		elseif node.param2 ~= 0 then
 			minetest.set_node(pos, {name = "default:torch_wall",
 				param2 = node.param2})
 		end
