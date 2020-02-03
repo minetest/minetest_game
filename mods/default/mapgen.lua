cbf5bcfdf27c1fd1e29d06fc79ab929df7da28e6
paramat <paramat@users.noreply.github.com>
2020-02-03 19:17:29 +0000

    Fix missing papyrus in savanna, add a dry dirt version
    Papyrus was missing from the dry dirt 'savanna_shore' biome due to
    only being placed on dirt. A mistake made when changing savanna
    dirt to dry dirt.
    
    Rename existing papyrus schematic to 'papyrus_on_dirt' and only
    place in the 'rainforest_swamp' biome.
    Add a new papyrus schematic 'papyrus_on_dry_dirt', placed in the
    'savanna' biome.

--- mods/default/mapgen.lua
+++ mods/default/mapgen.lua
@@ -2143,8 +2143,10 @@ function default.register_decorations()
 
 	-- Papyrus
 
+	-- Dirt version for rainforest swamp
+
 	minetest.register_decoration({
-		name = "default:papyrus",
+		name = "default:papyrus_on_dirt",
 		deco_type = "schematic",
 		place_on = {"default:dirt"},
 		sidelen = 16,
@@ -2156,10 +2158,32 @@ function default.register_decorations()
 			octaves = 3,
 			persist = 0.7
 		},
-		biomes = {"savanna_shore", "rainforest_swamp"},
+		biomes = {"rainforest_swamp"},
 		y_max = 0,
 		y_min = 0,
-		schematic = minetest.get_modpath("default") .. "/schematics/papyrus.mts",
+		schematic = minetest.get_modpath("default") .. "/schematics/papyrus_on_dirt.mts",
+	})
+
+	-- Dry dirt version for savanna shore
+
+	minetest.register_decoration({
+		name = "default:papyrus_on_dry_dirt",
+		deco_type = "schematic",
+		place_on = {"default:dry_dirt"},
+		sidelen = 16,
+		noise_params = {
+			offset = -0.3,
+			scale = 0.7,
+			spread = {x = 200, y = 200, z = 200},
+			seed = 354,
+			octaves = 3,
+			persist = 0.7
+		},
+		biomes = {"savanna_shore"},
+		y_max = 0,
+		y_min = 0,
+		schematic = minetest.get_modpath("default") ..
+			"/schematics/papyrus_on_dry_dirt.mts",
 	})
 
 	-- Bush
