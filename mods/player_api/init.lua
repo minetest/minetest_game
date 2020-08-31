943c1a242d1ed0d829765d4a605a842dd2564125
paramat <paramat@users.noreply.github.com>
2020-08-31 23:30:18 +0100

    Player_api: Various maintenance
    Clear 'player_sneak' and 'player_api.player_attached' table values
    when player leaves.
    Remove unnecessary commas and whitespace.
    Fix table name in 'game_api.txt'.
    Clean up documentation in 'game_api.txt'.

--- mods/player_api/init.lua
+++ mods/player_api/init.lua
@@ -5,7 +5,7 @@ dofile(minetest.get_modpath("player_api") .. "/api.lua")
 -- Default player appearance
 player_api.register_model("character.b3d", {
 	animation_speed = 30,
-	textures = {"character.png", },
+	textures = {"character.png"},
 	animations = {
 		-- Standard animations.
 		stand     = {x = 0,   y = 79},
