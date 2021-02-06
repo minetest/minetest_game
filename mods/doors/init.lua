08b13c9efb2101c8157b6fe3f280ada808e7bed5
sfan5 <sfan5@live.de>
2021-02-06 15:40:26 +0100

    Fix deprecated alpha / use_texture_alpha use

--- mods/doors/init.lua
+++ mods/doors/init.lua
@@ -436,6 +436,7 @@ function doors.register(name, def)
 	def.buildable_to = false
 	def.selection_box = {type = "fixed", fixed = {-1/2,-1/2,-1/2,1/2,3/2,-6/16}}
 	def.collision_box = {type = "fixed", fixed = {-1/2,-1/2,-1/2,1/2,3/2,-6/16}}
+	def.use_texture_alpha = "clip"
 
 	def.mesh = "door_a.obj"
 	minetest.register_node(":" .. name .. "_a", def)
@@ -581,6 +582,7 @@ function doors.register_trapdoor(name, def)
 	def.paramtype = "light"
 	def.paramtype2 = "facedir"
 	def.is_ground_content = false
+	def.use_texture_alpha = "clip"
 
 	if def.protected then
 		def.can_dig = can_dig_door
