1de58d0011c065b8175a062a3d8acff72d83d9be
Lars Mueller <appgurulars@gmx.de>
2022-01-14 21:15:18 +0100

    Fix changing player textures

--- mods/player_api/api.lua
+++ mods/player_api/api.lua
@@ -99,13 +99,13 @@ end
 function player_api.get_textures(player)
 	local player_data = get_player_data(player)
 	local model = models[player_data.model]
-	return model and model.textures or player_data.textures
+	return assert(player_data.textures or (model and model.textures))
 end
 
 function player_api.set_textures(player, textures)
 	local player_data = get_player_data(player)
 	local model = models[player_data.model]
-	local new_textures = model and model.textures or textures
+	local new_textures = assert(textures or (model and model.textures))
 	player_data.textures = new_textures
 	player:set_properties({textures = new_textures})
 end
