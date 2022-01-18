acc918436afec1a629891e8d95cf3b3621728a31
sfan5 <sfan5@live.de>
2022-01-18 19:18:44 +0100

    Override local_animation settings for certain animations
    This makes the lay/sit animation work on the local client (in third-person).

--- mods/player_api/init.lua
+++ mods/player_api/init.lua
@@ -7,11 +7,13 @@ player_api.register_model("character.b3d", {
 	animations = {
 		-- Standard animations.
 		stand     = {x = 0,   y = 79},
-		lay       = {x = 162, y = 166, collisionbox = {-0.6, 0.0, -0.6, 0.6, 0.3, 0.6}, eye_height = 0.3},
+		lay       = {x = 162, y = 166, eye_height = 0.3, override_local = true,
+			collisionbox = {-0.6, 0.0, -0.6, 0.6, 0.3, 0.6}},
 		walk      = {x = 168, y = 187},
 		mine      = {x = 189, y = 198},
 		walk_mine = {x = 200, y = 219},
-		sit       = {x = 81,  y = 160, collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.0, 0.3}, eye_height = 0.8}
+		sit       = {x = 81,  y = 160, eye_height = 0.8, override_local = true,
+			collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.0, 0.3}}
 	},
 	collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
 	stepheight = 0.6,
