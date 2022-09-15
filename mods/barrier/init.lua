694f7d292224625b05ec473924f74de0513110a3
Stvk imension <76146430+oong819@users.noreply.github.com>
2022-09-15 09:51:41 +0700

    Add barrier

--- mods/barrier/init.lua
+++ mods/barrier/init.lua
@@ -1,4 +1,22 @@
 --Made by Stvk
 
 --get translate
-minetest.get_
+local S = minetest.get_translator("barrier")
+
+--the barrier
+
+minetest.register_node(":barrier", {
+	description = S("Barrier"),
+	inventory_image = "ignore.png^air.png",
+	wield_image = "ignore.png^air.png",
+	drawtype = "airlike",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = true,
+	pointable = false,
+	diggable = false
+	buildable_to = true,
+	floodable: false,
+	drop = "",
+	groups = {not_in_creative_inventory = 1},
+})
