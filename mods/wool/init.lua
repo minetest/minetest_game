508a9070a0c2cfb95f3412159ea0a8a7b7d1de99
Lars Mueller <appgurulars@gmx.de>
2022-09-20 21:43:56 +0200

    Add color_* groups to wool

--- mods/wool/init.lua
+++ mods/wool/init.lua
@@ -8,19 +8,21 @@ local dyes = dye.dyes
 for i = 1, #dyes do
 	local name, desc = unpack(dyes[i])
 
+	local color_group = "color_" .. name
+
 	minetest.register_node("wool:" .. name, {
 		description = S(desc .. " Wool"),
 		tiles = {"wool_" .. name .. ".png"},
 		is_ground_content = false,
 		groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3,
-				flammable = 3, wool = 1},
+				flammable = 3, wool = 1, [color_group] = 1},
 		sounds = default.node_sound_defaults(),
 	})
 
 	minetest.register_craft{
 		type = "shapeless",
 		output = "wool:" .. name,
-		recipe = {"group:dye,color_" .. name, "group:wool"},
+		recipe = {"group:dye," .. color_group, "group:wool"},
 	}
 end
 
