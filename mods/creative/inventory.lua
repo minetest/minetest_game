72cb7dbdb2954922f52c14591ec99658f68dd8cf
Lars Mueller <appgurulars@gmx.de>
2023-03-31 08:06:26 +0200

    Fix patterns

--- mods/creative/inventory.lua
+++ mods/creative/inventory.lua
@@ -196,7 +196,7 @@ function creative.register_tab(name, title, items)
 					and fields.creative_filter then
 				inv.start_i = 0
 				inv.filter = fields.creative_filter:sub(1, 128) -- truncate to a sane length
-						:gsub("[%z-\8\11-\31\127]", "") -- strip naughty control characters (keeps \t and \n)
+						:gsub("[%z\1-\8\11-\31\127]", "") -- strip naughty control characters (keeps \t and \n)
 						:lower() -- search is case insensitive
 				sfinv.set_player_inventory_formspec(player, context)
 			elseif not fields.quit then
