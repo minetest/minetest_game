4d69915d8b76e688e6748309e7c038ee66b0383a
José Douglas da Silva Souza <44056294+JoseDouglas26@users.noreply.github.com>
2023-12-23 22:02:57 -0300

    Create api.lua

---
+++ mods/fireflies/api.lua
@@ -0,0 +1,64 @@
+-- fireflies/api.lua
+
+fireflies = {}
+-- If necessary, add other functions related to fireflies here
+
+--------------------------------------------------------------
+-- Global function to record the decoration of fireflies. 
+-- Perhaps other parameters can be added to improve decoration registration control.
+function fireflies.register_decoration(biomes, place_on)
+  local mg_name = minetest.get_mapgen_setting("mg_name")
+  
+  if mg_name == "v6" then
+		minetest.register_decoration({
+			name = "fireflies:firefly_low",
+			deco_type = "simple",
+			place_on = place_on,
+			place_offset_y = 2,
+			sidelen = 80,
+			fill_ratio = 0.0002,
+			y_max = 31000,
+			y_min = 1,
+			decoration = "fireflies:hidden_firefly",
+		})
+
+		minetest.register_decoration({
+			name = "fireflies:firefly_high",
+			deco_type = "simple",
+			place_on = place_on,
+			place_offset_y = 3,
+			sidelen = 80,
+			fill_ratio = 0.0002,
+			y_max = 31000,
+			y_min = 1,
+			decoration = "fireflies:hidden_firefly",
+		})
+    
+  else
+		minetest.register_decoration({
+			name = "fireflies:firefly_low",
+			deco_type = "simple",
+			place_on = place_on,
+			place_offset_y = 2,
+			sidelen = 80,
+			fill_ratio = 0.0005,
+			biomes = biomes,
+			y_max = 31000,
+			y_min = -1,
+			decoration = "fireflies:hidden_firefly",
+		})
+    
+		minetest.register_decoration({
+			name = "fireflies:firefly_high",
+			deco_type = "simple",
+			place_on = place_on,
+			place_offset_y = 3,
+			sidelen = 80,
+			fill_ratio = 0.0005,
+			biomes = biomes,
+			y_max = 31000,
+			y_min = -1,
+			decoration = "fireflies:hidden_firefly",
+		})
+  end
+end
