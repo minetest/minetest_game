b8b3acac04c3237305738c34a40927694eb9aea2
José Douglas da Silva Souza <44056294+JoseDouglas26@users.noreply.github.com>
2024-05-27 06:00:06 -0300

    Swap tbl to sounds

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -2,149 +2,149 @@
 -- Sounds
 --
 
-function default.node_sound_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "", gain = 1.0}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_dug_node", gain = 0.25}
-	tbl.place = tbl.place or
+	sounds.place = sounds.place or
 			{name = "default_place_node_hard", gain = 1.0}
-	return tbl
+	return sounds
 end
 
-function default.node_sound_stone_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_stone_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_hard_footstep", gain = 0.2}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_hard_footstep", gain = 1.0}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_dirt_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_dirt_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_dirt_footstep", gain = 0.25}
-	tbl.dig = tbl.dig or
+	sounds.dig = sounds.dig or
 			{name = "default_dig_crumbly", gain = 0.4}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_dirt_footstep", gain = 1.0}
-	tbl.place = tbl.place or
+	sounds.place = sounds.place or
 			{name = "default_place_node", gain = 1.0}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_sand_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_sand_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_sand_footstep", gain = 0.05}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_sand_footstep", gain = 0.15}
-	tbl.place = tbl.place or
+	sounds.place = sounds.place or
 			{name = "default_place_node", gain = 1.0}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_gravel_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_gravel_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_gravel_footstep", gain = 0.25}
-	tbl.dig = tbl.dig or
+	sounds.dig = sounds.dig or
 			{name = "default_gravel_dig", gain = 0.35}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_gravel_dug", gain = 1.0}
-	tbl.place = tbl.place or
+	sounds.place = sounds.place or
 			{name = "default_place_node", gain = 1.0}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_wood_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_wood_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_wood_footstep", gain = 0.15}
-	tbl.dig = tbl.dig or
+	sounds.dig = sounds.dig or
 			{name = "default_dig_choppy", gain = 0.4}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_wood_footstep", gain = 1.0}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_leaves_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_leaves_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_grass_footstep", gain = 0.45}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_grass_footstep", gain = 0.7}
-	tbl.place = tbl.place or
+	sounds.place = sounds.place or
 			{name = "default_place_node", gain = 1.0}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_glass_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_glass_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_glass_footstep", gain = 0.3}
-	tbl.dig = tbl.dig or
+	sounds.dig = sounds.dig or
 			{name = "default_glass_footstep", gain = 0.5}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_break_glass", gain = 1.0}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_ice_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_ice_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_ice_footstep", gain = 0.15}
-	tbl.dig = tbl.dig or
+	sounds.dig = sounds.dig or
 			{name = "default_ice_dig", gain = 0.5}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_ice_dug", gain = 0.5}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_metal_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_metal_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_metal_footstep", gain = 0.2}
-	tbl.dig = tbl.dig or
+	sounds.dig = sounds.dig or
 			{name = "default_dig_metal", gain = 0.5}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_dug_metal", gain = 0.5}
-	tbl.place = tbl.place or
+	sounds.place = sounds.place or
 			{name = "default_place_node_metal", gain = 0.5}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_water_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_water_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_water_footstep", gain = 0.2}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
-function default.node_sound_snow_defaults(tbl)
-	tbl = tbl or {}
-	tbl.footstep = tbl.footstep or
+function default.node_sound_snow_defaults(sounds)
+	sounds = sounds or {}
+	sounds.footstep = sounds.footstep or
 			{name = "default_snow_footstep", gain = 0.2}
-	tbl.dig = tbl.dig or
+	sounds.dig = sounds.dig or
 			{name = "default_snow_footstep", gain = 0.3}
-	tbl.dug = tbl.dug or
+	sounds.dug = sounds.dug or
 			{name = "default_snow_footstep", gain = 0.3}
-	tbl.place = tbl.place or
+	sounds.place = sounds.place or
 			{name = "default_place_node", gain = 1.0}
-	default.node_sound_defaults(tbl)
-	return tbl
+	default.node_sound_defaults(sounds)
+	return sounds
 end
 
 
