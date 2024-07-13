621c0f70ceef55089fca9b03d0eb54c6009ffef3
1F616EMO <root@1f616emo.xyz>
2024-07-13 20:00:35 +0800

    Log furnace inventory actions

--- mods/default/furnace.lua
+++ mods/default/furnace.lua
@@ -329,7 +329,12 @@ end
 -- Node definitions
 --
 
-minetest.register_node("default:furnace", {
+local function apply_logger(def)
+	default.set_inventory_action_loggers(def, "furnace")
+	return def
+end
+
+minetest.register_node("default:furnace", apply_logger({
 	description = S("Furnace"),
 	tiles = {
 		"default_furnace_top.png", "default_furnace_bottom.png",
@@ -379,9 +384,9 @@ minetest.register_node("default:furnace", {
 	allow_metadata_inventory_put = allow_metadata_inventory_put,
 	allow_metadata_inventory_move = allow_metadata_inventory_move,
 	allow_metadata_inventory_take = allow_metadata_inventory_take,
-})
+}))
 
-minetest.register_node("default:furnace_active", {
+minetest.register_node("default:furnace_active", apply_logger({
 	description = S("Furnace"),
 	tiles = {
 		"default_furnace_top.png", "default_furnace_bottom.png",
@@ -415,7 +420,7 @@ minetest.register_node("default:furnace_active", {
 	allow_metadata_inventory_put = allow_metadata_inventory_put,
 	allow_metadata_inventory_move = allow_metadata_inventory_move,
 	allow_metadata_inventory_take = allow_metadata_inventory_take,
-})
+}))
 
 minetest.register_craft({
 	output = "default:furnace",
