6da5e947f0435cd05d4d58bbe9d55bafe05d093d
EGYT5453 <113429882+EGYT5453@users.noreply.github.com>
2025-01-05 20:19:14 +0200

    Fix optimize_textures.sh

--- utils/optimize_textures.sh
+++ utils/optimize_textures.sh
@@ -3,4 +3,4 @@
 # Colors with 0 alpha need to be preserved, because opaque leaves ignore alpha.
 # For that purpose, the use of indexed colors is disabled (-nc).
 
-find -name '../*.png' -print0 | xargs -0 optipng -o7 -zm1-9 -nc -strip all -clobber
+find .. -name '*.png' -print0 | xargs -0 optipng -o7 -zm1-9 -nc -strip all -clobber
